35ee35bfebcd87f80217c61400fcb308bb82e0aa
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-10-17 23:58:34 -0700

    Change the selector

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -2,20 +2,20 @@
 
 (function() {
     "use strict";
-    
-/**
-   * Intro type effect
-   */
-const typed = select('.typed')
-if (typed) {
-  let typed_strings = typed.getAttribute('data-typed-items')
-  typed_strings = typed_strings.split(',')
-  new Typed('.typed', {
-    strings: typed_strings,
-    loop: true,
-    typeSpeed: 100,
-    backSpeed: 50,
-    backDelay: 2000
-  });
-}
-});
+
+    /**
+     * Intro type effect
+     */
+    const typed = document.querySelector('.typed'); // Use `document.querySelector` instead of `select`
+    if (typed) {
+        let typed_strings = typed.getAttribute('data-typed-items');
+        typed_strings = typed_strings.split(',');
+        new Typed('.typed', {
+            strings: typed_strings,
+            loop: true,
+            typeSpeed: 100,
+            backSpeed: 50,
+            backDelay: 2000
+        });
+    }
+})();
