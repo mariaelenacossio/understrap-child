4b63893547a69ba7912b39123473456d3d7897cd
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-10-18 00:36:07 -0700

    Change hero image

--- home.php
+++ home.php
@@ -24,7 +24,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 <div class="container col-xxl-8 px-4 py-5">
     <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
       <div class="col-10 col-sm-8 col-lg-6">
-        <img src="bootstrap-themes.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+        <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/10/design-system-phone-mockup-5.png" alt="profile-img" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
       </div>
       <div class="col-lg-6">
         <h1 class="display-5 fw-bold lh-1 mb-3">Responsive left-aligned hero with image</h1>
