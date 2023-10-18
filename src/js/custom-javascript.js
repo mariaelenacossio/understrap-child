813104d55437e6b9017e315005fd0d499affd73b
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-10-18 00:01:31 -0700

    Change function

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -6,16 +6,15 @@
     /**
      * Intro type effect
      */
-    const typed = document.querySelector('.typed'); // Use `document.querySelector` instead of `select`
-    if (typed) {
-        let typed_strings = typed.getAttribute('data-typed-items');
-        typed_strings = typed_strings.split(',');
-        new Typed('.typed', {
-            strings: typed_strings,
-            loop: true,
-            typeSpeed: 100,
-            backSpeed: 50,
-            backDelay: 2000
-        });
-    }
-})();
+    const options = {
+        stringsElement: '.typed',
+        typeSpeed: 100,       // Typing speed in milliseconds
+        backSpeed: 50,        // Backspacing speed in milliseconds
+        backDelay: 2000,      // Delay before starting to backspace
+        startDelay: 500,      // Delay before typing starts
+        loop: true,           // Loop the animation
+        showCursor: true,     // Show the blinking cursor
+      };
+    
+      const typed = new Typed('.typed', options);
+});
