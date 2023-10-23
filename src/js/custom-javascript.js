79dd554083810f83350ac8a62fbf7640c07b5d2b
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-10-22 21:34:45 -0700

    Add script for tooltip

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -1,20 +1,9 @@
 // Add your custom JS here.
-
-(function() {
-    "use strict";
-
-    /**
-     * Intro type effect
-     */
-    const options = {
-        stringsElement: '.typed',
-        typeSpeed: 100,       // Typing speed in milliseconds
-        backSpeed: 50,        // Backspacing speed in milliseconds
-        backDelay: 2000,      // Delay before starting to backspace
-        startDelay: 500,      // Delay before typing starts
-        loop: true,           // Loop the animation
-        showCursor: true,     // Show the blinking cursor
-      };
-    
-      const typed = new Typed('.typed', options);
-});
+/* global bootstrap: false */
+(function () {
+  'use strict'
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+    new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})()
