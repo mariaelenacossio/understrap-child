31526cf8a94bb1d639c09c436ba53268f46ff6db
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-10-22 21:35:58 -0700

    Remove changes

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -1,9 +1,1 @@
 // Add your custom JS here.
-/* global bootstrap: false */
-(function () {
-  'use strict'
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
-    new bootstrap.Tooltip(tooltipTriggerEl)
-  })
-})()
