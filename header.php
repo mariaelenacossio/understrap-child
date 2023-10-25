9f4f2f1a0bf33c87164177f74bf31ec45fad4264
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-10-24 23:32:07 -0700

    Remove class fro btm svg

--- header.php
+++ header.php
@@ -71,7 +71,7 @@ $navbar_type       = get_theme_mod( 'understrap_navbar_type', 'collapse' );
 				</a>
 				</li>
 				<li class="nav-item">
-				<a href="https://www.linkedin.com/in/mariaelena-cossio-clark-320ba416b/" class="nav-link py-3 border-bottom" title="Github" data-bs-toggle="tooltip" data-bs-placement="right">
+				<a href="https://www.linkedin.com/in/mariaelena-cossio-clark-320ba416b/" class="nav-link py-3 border-bottom" title="Linkedin" data-bs-toggle="tooltip" data-bs-placement="right">
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" aria-label="Linkedin">
 					<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
 					<path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
@@ -108,7 +108,7 @@ $navbar_type       = get_theme_mod( 'understrap_navbar_type', 'collapse' );
 			<div class="nav d-flex">
 			<!-- <div class="nav-item"> -->
 				<a href="#home" class="nav-link active py-3" aria-current="page" title="Home" data-bs-toggle="tooltip" data-bs-placement="right">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="i-color home" aria-label="Home">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" aria-label="Home">
 					<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
 					<path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>        
 				</a>
