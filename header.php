5c2c351e7884085b0f77ce7cf24d5cfb5fc29915
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-10-24 21:43:21 -0700

    Remove class icon

--- header.php
+++ header.php
@@ -28,7 +28,7 @@ $navbar_type       = get_theme_mod( 'understrap_navbar_type', 'collapse' );
 
 	<!-- ******************* The Navbar Area ******************* -->
 	<header  class="side-bar img-shadow" id="wrapper-navbar">
-	<div class="d-flex flex-column flex-shrink-0" style="width: 4.5rem;">
+	<div class="d-flex flex-column flex-shrink-0 align-items-center" style="width: 4rem;">
 		<!-- <a href="/" class="d-block p-3 link-dark text-decoration-none" title="Icon-only" data-bs-toggle="tooltip" data-bs-placement="right"> -->
 			<!-- <svg class="bi" width="40" height="32"><use xlink:href="#bootstrap"/></svg> -->
 			<!-- <span class="visually-hidden">Icon-only</span> -->
@@ -42,7 +42,7 @@ $navbar_type       = get_theme_mod( 'understrap_navbar_type', 'collapse' );
 					<path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>        
 				</a>
 				</li>
-				<li class="nav-item icon icon-fill">
+				<li class="nav-item icon-fill">
 				<a href="#about" class="nav-link py-3 border-bottom" title="About" data-bs-toggle="tooltip" data-bs-placement="right">
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="i-color" aria-label="About">
 					<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
