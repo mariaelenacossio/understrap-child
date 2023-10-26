545072acf40ad6a5bd2dcd1ea09eac9084d5c254
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-10-26 10:59:23 -0700

    Test cards

--- home.php
+++ home.php
@@ -144,6 +144,26 @@ $container = get_theme_mod( 'understrap_container_type' );
 				</section>
 				
 
+				<!--This is a Test-->
+				<section class="portfolio-cards">
+            <div class="row row-cols-1 row-cols-md-2 g-4">
+                <div class="col">
+                    <div class="card custom-card">
+                        <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/10/design-system-phone-mockup-5.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <div class="card-buttons">
+                                <button class="card-button">View Site</button>
+                                <button class="card-button">Case Study</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Add more cards with similar structure here -->
+            </div>
+        </section>
+
 				<!-- ?php
 				if ( have_posts() ) {
 					// Start the Loop.
