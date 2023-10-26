3c2b3835b2c06515a7f7c2ef6b7de6fe2467b7e8
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-10-26 01:45:40 -0700

    Add class footer to main div

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
 
-<div class="wrapper" id="wrapper-footer">
+<div class="footer wrapper" id="wrapper-footer">
 
 <footer class="text-light p-5">
     <div class="container">
