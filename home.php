8f07c0441da5e7e14bccad9950ce61a28ad2dd3c
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-10-31 14:16:49 -0700

    Change link for view site btn

--- home.php
+++ home.php
@@ -204,8 +204,7 @@ $container = get_theme_mod( 'understrap_container_type' );
               <a href="#!">
                 <span class="badge rounded-pill text-bg-warning position-absolute top-10px end-10px">HTML/CSS/Bootstrap</span>
                 <img class="img-fluid rounded w-100 hcf-of-cover hcf-op-center hcf-ih-250 hcf-ih-md-400" loading="lazy" src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/10/website-project-1.png" alt="">
-				<a class="btn btn-primary" href="https://mariaelenacossio.github.io/losquequitosmx.github.io/" target="_blank">View Site</a>
-				</a>
+              </a>
             </div>
           </div>
           <div class="col-xs-12 col-md-6">
@@ -214,6 +213,7 @@ $container = get_theme_mod( 'understrap_container_type' );
               <h2 class="h1 mb-3"><a class="link-dark text-decoration-none" href="#!">Local E-commerce</a></h2>
               <p class="mb-4">User-friendly website, built with Bootstrap, offers a seamless and secure shopping experience</p>
               <a class="btn btn-primary" href="#!" target="_self">Read More</a>
+			  <a class="btn btn-primary" href="https://mariaelenacossio.github.io/losquequitosmx.github.io/" target="_blank">View Site</a>
             </div>
           </div>
         </div>
