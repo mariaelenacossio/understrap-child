166ddaf3baf4c1bac578314f0d9d85b13e02bb54
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-10-31 15:10:50 -0700

    Add contact form

--- home.php
+++ home.php
@@ -262,7 +262,83 @@ $container = get_theme_mod( 'understrap_container_type' );
       </div>
     </div>
   </div>
-</section>		
+</section>
+
+<!--Section: Contact v.3-->
+<section class="mb-4">
+    <div class="container">
+
+        <!--Section heading-->
+        <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+
+        <!--Section description-->
+        <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within a matter of hours to help you.</p>
+
+        <div class="row">
+
+            <!--Grid column for image-->
+            <div class="col-md-6 mb-4">
+                <div class="contact-image" style="background-image: url('your-image.jpg'); border: 1px solid #ccc; border-radius: 25px;"></div>
+            </div>
+
+            <!--Grid column for the form-->
+            <div class="col-md-6 mb-md-0 mb-5">
+                <form id="contact-form" name="contact-form" action="mail.php" method="POST" style="border: 1px solid #ccc; border-radius: 25px; padding: 15px;">
+
+                    <!--Grid row-->
+                    <div class="row">
+
+                        <!--Grid column for name-->
+                        <div class="col-md-6">
+                            <div class="md-form mb-0">
+                                <input type="text" id="name" name="name" class="form-control">
+                                <label for="name" class="">Your name</label>
+                            </div>
+                        </div>
+
+                        <!--Grid column for email-->
+                        <div class="col-md-6">
+                            <div class="md-form mb-0">
+                                <input type="text" id="email" name="email" class="form-control">
+                                <label for="email" class="">Your email</label>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Grid row-->
+
+                    <!--Grid row for subject-->
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="md-form mb-0">
+                                <input type="text" id="subject" name="subject" class="form-control">
+                                <label for="subject" class="">Subject</label>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Grid row for subject-->
+
+                    <!--Grid row for message-->
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="md-form">
+                                <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                                <label for="message">Your message</label>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Grid row for message-->
+                </form>
+
+                <div class="text-center text-md-left">
+                    <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+                </div>
+                <div class="status"></div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Section: Contact v.3-->
+
 				<!-- ?php
 				if ( have_posts() ) {
 					// Start the Loop.
