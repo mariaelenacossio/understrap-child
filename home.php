dbebed639c525b5d208b7fff8438bd28ca34d3c4
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-10-31 15:48:01 -0700

    Uncomment index wrapper

--- home.php
+++ home.php
@@ -89,7 +89,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 	?php get_template_part( 'global-templates/hero' ); ?>
 ?php endif; ?-->
 
-<!-- <div class="wrapper" id="index-wrapper"> -->
+<div class="wrapper" id="index-wrapper">
 
 
 	<!-- <div class="?php echo esc_attr( $container ); ?>" id="content" tabindex="-1"> -->
@@ -369,7 +369,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 	<!-- </div>#content -->
 
-<!-- </div>#index-wrapper -->
+</div> <!--#index-wrapper-->
 <?php
 get_footer();
 ?>
