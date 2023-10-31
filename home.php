ff5f6f58724a7361e4e3ea76b97688073961c66b
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-10-31 16:09:26 -0700

    Add div for portfolio cards

--- home.php
+++ home.php
@@ -191,9 +191,11 @@ $container = get_theme_mod( 'understrap_container_type' );
               <!-- <p class="text-secondary mb-1">Nov 11, 2022</p> -->
               <h2 class="h1 mb-3"><a class="link-dark text-decoration-none" href="#!">Design System</a></h2>
               <p class="mb-4">Elevating User Experience with a Design System.</p>
+			  <div class="portfolio-btn">
               <a class="btn btn-primary" href="#!" target="_self">Read More</a>
 			  <a class="btn btn-primary" href="https://mariaelenacossio.github.io/DGL-309-design-system/" target="_blank">View Site</a>
-            </div>
+			  </div><!--.div portfolio btn-->
+			</div>
           </div>
         </div>
       </div>
