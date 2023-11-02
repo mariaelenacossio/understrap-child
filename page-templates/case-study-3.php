95a29e4025399898f477c9a7e3f0564692f7b0f9
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-02 02:41:04 -0700

    Add hero img

--- page-templates/case-study-3.php
+++ page-templates/case-study-3.php
@@ -44,7 +44,7 @@ get_header();
         <div class="projects-overflow mt-md-10 mb-10 mb-lg-15">
           <div class="project item">
             <div class="row">
-              <figure class="col-lg-8 col-xl-7 offset-xl-1 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/10/website-project-1.png" alt="" /></figure>
+              <figure class="col-lg-8 col-xl-7 offset-xl-1 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/10/website-project-2.png" alt="" /></figure>
               <div class="project-details d-flex justify-content-center flex-column" style="right: 10%; bottom: 25%;">
                 <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
                   <div class="card-body">
