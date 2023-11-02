e1a269e1afdf73f08fbc0bdf32ba920002f8fe0b
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-02 01:21:48 -0700

    Add info for case study

--- page-templates/case-study.php
+++ page-templates/case-study.php
@@ -30,7 +30,7 @@ get_header();
         <div class="row">
           <div class="col-md-8 col-lg-7 col-xl-6 col-xxl-5 mx-auto">
             <h1 class="display-1 mb-3">Case Study</h1>
-            <p class="lead fs-lg px-lg-10 px-xxl-8">Design System.</p>
+            <p class="lead fs-lg px-lg-10 px-xxl-8">Design System</p>
           </div>
           <!-- /column -->
         </div>
@@ -54,15 +54,8 @@ get_header();
                     </div>
                     <!-- /.post-header -->
                     <div class="post-content">
-                      <p>BEYOND is a skincare brand. Its identity is embodied in its design system. 
-                         The  colour palette reflects the brand’s identity by establishing consistent 
-                         visual language and user experience across different pages and components. 
-                         Typography ensures readability and legibility across all platforms as well, 
-                         the guidelines established are based on the company values which are sustainability, 
-                         transparency, and ethical practices of products with a purpose for your skin as well, 
-                         as the environment.
-                         The website sample maintains a consistent resemblance to the elements, components,
-                         and materials that were constructed solely using HTML, CSS, and JS.</p>
+                      <p>BEYOND skincare brand's design system emphasizes brand identity, 
+                        sustainability, and consistency in color, typography, and user experience.</p>
                       <a href="#" class="more hover link-purple">See Project</a>
                     </div>
                     <!-- /.post-content -->
@@ -78,17 +71,18 @@ get_header();
           <!-- /.project -->
           <div class="project item">
             <div class="row">
-              <figure class="col-lg-7 offset-lg-5 col-xl-6 offset-xl-5 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/10/design-system-phone-mockup-5.png" alt="" /></figure>
+              <figure class="col-lg-7 offset-lg-5 col-xl-6 offset-xl-5 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/10/design-system-desktop-mockup-3@2x.png" alt="" /></figure>
               <div class="project-details d-flex justify-content-center flex-column" style="left: 18%; bottom: 25%;">
                 <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
                   <div class="card-body">
                     <div class="post-header">
-                      <div class="post-category text-line text-leaf mb-3">Coffee</div>
-                      <h2 class="post-title mb-3">Mollis Ipsum Mattis</h2>
+                      <div class="post-category text-line text-leaf mb-3">Research and Discovery</div>
+                      <h2 class="post-title mb-3">Define the Scope</h2>
                     </div>
                     <!-- /.post-header -->
                     <div class="post-content">
-                      <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam id dolor id nibh ultricies vehicula.</p>
+                      <p>In this step, I identified the needs and goals that the system will serve the target audience, 
+                        and the elements and components required to meet the user needs and brand identity.</p>
                       <a href="#" class="more hover link-leaf">See Project</a>
                     </div>
                     <!-- /.post-content -->
@@ -104,18 +98,18 @@ get_header();
           <!-- /.project -->
           <div class="project item">
             <div class="row">
-              <figure class="col-lg-9 col-xl-7 offset-xl-2 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/10/design-system-phone-mockup-5.png" alt="" /></figure>
+              <figure class="col-lg-9 col-xl-7 offset-xl-2 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/11/Desktop-Home.png" alt="" /></figure>
               <div class="project-details d-flex justify-content-center flex-column" style="right: 3%; bottom: 25%;">
                 <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
                   <div class="card-body">
                     <div class="post-header">
-                      <div class="post-category text-line text-violet mb-3">Still Life</div>
-                      <h2 class="post-title mb-3">Ipsum Ultricies Cursus</h2>
+                      <div class="post-category text-line text-violet mb-3">Ideation and Design</div>
+                      <h2 class="post-title mb-3">Wireframes</h2>
                     </div>
                     <!-- /.post-header -->
                     <div class="post-content">
-                      <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
-                      <a href="#" class="more hover link-violet">See Project</a>
+                      <p>I used Adobe Xd to create the layout of mobile and desktop screens for the design system and the sample website based on the mood board I put together to convey the overall look, feel, and style.</p>
+                      <a href="#" class="more hover link-violet">Xd Wireframes</a>
                     </div>
                     <!-- /.post-content -->
                   </div>
@@ -130,18 +124,21 @@ get_header();
           <!-- /.project -->
           <div class="project item">
             <div class="row">
-              <figure class="col-lg-9 offset-lg-3 col-xl-7 offset-xl-4 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/10/design-system-phone-mockup-5.png" alt="" /></figure>
+              <figure class="col-lg-9 offset-lg-3 col-xl-7 offset-xl-4 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/11/mariaelena-cossio-social-media-mockup-mobile-1@2x.png" alt="" /></figure>
               <div class="project-details d-flex justify-content-center flex-column" style="left: 12%; bottom: 25%;">
                 <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
                   <div class="card-body">
                     <div class="post-header">
-                      <div class="post-category text-line text-yellow mb-3">Product</div>
-                      <h2 class="post-title mb-3">Sollicitudin Ornare Porta</h2>
+                      <div class="post-category text-line text-yellow mb-3">Prototyping</div>
+                      <h2 class="post-title mb-3">Build, Build And Keep Building</h2>
                     </div>
                     <!-- /.post-header -->
                     <div class="post-content">
-                      <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
-                      <a href="#" class="more hover link-yellow">See Project</a>
+                      <p>During the development process of my sample website, 
+                        I utilized HTML for building and CSS for styling to create pages that 
+                        showcased the guidelines and format of the components and layout. 
+                        Ultimately, I displayed the finished product on the sample website.</p>
+                      <a href="https://github.com/mariaelenacossio/DGL-309-design-system" class="more hover link-yellow">Github Repository</a>
                     </div>
                     <!-- /.post-content -->
                   </div>
@@ -182,17 +179,19 @@ get_header();
           <!-- /.project -->
           <div class="project item">
             <div class="row">
-              <figure class="col-lg-9 offset-lg-3 col-xl-7 offset-xl-5 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/10/design-system-phone-mockup-5.png" alt="" /></figure>
+              <figure class="col-lg-9 offset-lg-3 col-xl-7 offset-xl-5 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/11/DS-test.png" alt="" /></figure>
               <div class="project-details d-flex justify-content-center flex-column ms-lg-n150 ms-xl-0" style="left: 18%; bottom: 25%;">
                 <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
                   <div class="card-body">
                     <div class="post-header">
-                      <div class="post-category text-line text-green mb-3">Workshop</div>
-                      <h2 class="post-title mb-3">Ipsum Mollis Vulputate</h2>
+                      <div class="post-category text-line text-green mb-3">Testing and Iteration</div>
+                      <h2 class="post-title mb-3">Test, Refine + Implement</h2>
                     </div>
                     <!-- /.post-header -->
                     <div class="post-content">
-                      <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+                      <p>The final step was to test the usability and 
+                        functionality with tools such as GTmetrix and WebAim in order to 
+                        identify the weak points to improve and refine them.</p>
                       <a href="#" class="more hover link-green">See Project</a>
                     </div>
                     <!-- /.post-content -->
@@ -213,13 +212,16 @@ get_header();
                 <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
                   <div class="card-body">
                     <div class="post-header">
-                      <div class="post-category text-line text-red mb-3">Concept</div>
-                      <h2 class="post-title mb-3">Porta Ornare Cras</h2>
+                      <div class="post-category text-line text-red mb-3">Conclusion</div>
+                      <h2 class="post-title mb-3">Challenges I faced</h2>
                     </div>
                     <!-- /.post-header -->
                     <div class="post-content">
-                      <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
-                      <a href="#" class="more hover link-red">See Project</a>
+                      <p>Building a design system is not a one-time process, 
+                        it requires continuous iteration and refinement. 
+                        Through regular meetings and walk-throughs with my peers and instructor, 
+                        I gained a fresh perspective on how to approach and provide feedback on our respective projects.</p>
+                      <a href="https://mariaelenacossio.github.io/DGL-309-design-system/" class="more hover link-red">See Project</a>
                     </div>
                     <!-- /.post-content -->
                   </div>
