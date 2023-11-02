6c91b483e0ca2b3e056da77205a0417d208771a7
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-02 04:47:17 -0700

    Adjust for the other cards

--- home.php
+++ home.php
@@ -158,7 +158,7 @@ $container = get_theme_mod( 'understrap_container_type' );
               <!-- <p class="text-secondary mb-1">Sep 17, 2022</p> -->
               <h2 class="h1 mb-3"><a class="link-dark text-decoration-none" href="#!">Business/E-commerce</a></h2>
               <p class="mb-4 text-truncate" style="max-height: 3.6em; overflow: hidden;">Crafting Elegance and User Experience with One Nail at a Time.</p>
-              <div class="portfolio-btn mt-3"> <!-- This code will apply only for screens not bigger than 390px -->
+              <div class="portfolio-btn mt-3 text-truncate" style="max-height: 3.6em; overflow: hidden;"> <!-- This code will apply only for screens not bigger than 390px -->
               <a class="btn btn-primary" href="https://mariaelena-cossioclark.com/beauty-by-amy/" target="_self">Read More</a>
 			  <a class="btn btn-primary" href="https://mariaelenacossio.github.io/beautybyamy.github.io/" target="_blank">View Site</a>
             </div>
@@ -181,7 +181,7 @@ $container = get_theme_mod( 'understrap_container_type' );
               <!-- <p class="text-secondary mb-1">Aug 23, 2022</p> -->
               <h2 class="h1 mb-3"><a class="link-dark text-decoration-none" href="#!">Web App</a></h2>
               <p class="mb-4">Seamless Journeys, Crafted with UX/UI Excellence.</p>
-              <div class="portfolio-btn mt-3"> <!-- This code will apply only for screens not bigger than 390px -->
+              <div class="portfolio-btn mt-3 text-truncate" style="max-height: 3.6em; overflow: hidden;"> <!-- This code will apply only for screens not bigger than 390px -->
               <a class="btn btn-primary" href="https://mariaelena-cossioclark.com/relocateme/" target="_self">Read More</a>
 			  <a class="btn btn-primary" href="https://xd.adobe.com/view/ef8d82b4-5695-4dfa-847f-2f7955ed59ef-bade/" target="_blank">View Site</a>
             </div>
