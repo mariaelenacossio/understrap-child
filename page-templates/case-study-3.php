4caf684a9b2c10c2bad6c189c317a0738a8ee0d0
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-02 02:40:33 -0700

    Add info for case study 3

---
+++ page-templates/case-study-3.php
@@ -0,0 +1,230 @@
+<?php
+/**
+ * Template Name: Case Study Page (3) Template
+ *
+ * Template for displaying a blank page.
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+    <?php
+get_header();
+?>
+</head>
+<body>
+
+<div class="interface">
+    <section class="wrapper bg-light">
+      <div class="container pt-10 pt-md-14 text-center">
+        <div class="row">
+          <div class="col-md-8 col-lg-7 col-xl-6 col-xxl-5 mx-auto">
+            <h1 class="display-1 mb-3">Case Study</h1>
+            <p class="lead fs-lg px-lg-10 px-xxl-8">Business E-commerce Website</p>
+          </div>
+          <!-- /column -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </section>
+    <!-- /section -->
+    <section class="wrapper bg-light">
+      <div class="container pt-9 pt-md-11 pb-14 pb-md-16">
+        <div class="projects-overflow mt-md-10 mb-10 mb-lg-15">
+          <div class="project item">
+            <div class="row">
+              <figure class="col-lg-8 col-xl-7 offset-xl-1 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/10/website-project-1.png" alt="" /></figure>
+              <div class="project-details d-flex justify-content-center flex-column" style="right: 10%; bottom: 25%;">
+                <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
+                  <div class="card-body">
+                    <div class="post-header">
+                      <div class="post-category text-line text-purple mb-3">Introduction</div>
+                      <h2 class="post-title mb-3">Beauty by Amy</h2>
+                    </div>
+                    <!-- /.post-header -->
+                    <div class="post-content">
+                      <p>Our independent nail technician sought a digital canvas to paint her artistry, 
+                        boost her client base, and lay the foundation for a future salon. 
+                        The journey began with passion and a blank HTML canvas.</p>
+                      <!-- <a href="#" class="more hover link-purple">See Project</a> -->
+                    </div>
+                    <!-- /.post-content -->
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+              <!-- /.project-details -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.project -->
+          <div class="project item">
+            <div class="row">
+              <figure class="col-lg-7 offset-lg-5 col-xl-6 offset-xl-5 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/11/traffic-cone.png" alt="" /></figure>
+              <div class="project-details d-flex justify-content-center flex-column" style="left: 18%; bottom: 25%;">
+                <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
+                  <div class="card-body">
+                    <div class="post-header">
+                      <div class="post-category text-line text-leaf mb-3">Research and Discovery</div>
+                      <h2 class="post-title mb-3">Coloring Within the Lines of Market Insights</h2>
+                    </div>
+                    <!-- /.post-header -->
+                    <div class="post-content">
+                      <p>We conducted a thorough study of local nail preferences, observed industry trends, 
+                        and dissected the online presence of competitors. Our analysis unveiled a unique blend 
+                        of artistic flair and technical skill.</p>
+                      <!-- <a href="#" class="more hover link-leaf">See Project</a> -->
+                    </div>
+                    <!-- /.post-content -->
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+              <!-- /.project-details -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.project -->
+          <div class="project item">
+            <div class="row">
+              <figure class="col-lg-9 col-xl-7 offset-xl-2 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/11/traffic-cone.png" alt="" /></figure>
+              <div class="project-details d-flex justify-content-center flex-column" style="right: 3%; bottom: 25%;">
+                <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
+                  <div class="card-body">
+                    <div class="post-header">
+                      <div class="post-category text-line text-violet mb-3">Ideation and Design</div>
+                      <h2 class="post-title mb-3">Nailin' the Aesthetic Appeal</h2>
+                    </div>
+                    <!-- /.post-header -->
+                    <div class="post-content">
+                      <p>Starting with basic HTML and CSS, we translated our client's artistry into a digital masterpiece. 
+                        The design was inspired by nail aesthetics, with color schemes and layouts mirroring the vivid diversity 
+                        of nail art.</p>
+                      <a href="#!" class="more hover link-violet">Wireframes</a>
+                    </div>
+                    <!-- /.post-content -->
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+              <!-- /.project-details -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.project -->
+          <div class="project item">
+            <div class="row">
+              <figure class="col-lg-9 offset-lg-3 col-xl-7 offset-xl-4 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/11/traffic-cone.png" alt="" /></figure>
+              <div class="project-details d-flex justify-content-center flex-column" style="left: 12%; bottom: 25%;">
+                <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
+                  <div class="card-body">
+                    <div class="post-header">
+                      <div class="post-category text-line text-yellow mb-3">Prototyping</div>
+                      <h2 class="post-title mb-3">From Blueprint to Browser</h2>
+                    </div>
+                    <!-- /.post-header -->
+                    <div class="post-content">
+                      <p>Prototyping began with wireframes to plan the website's structure, 
+                        ensuring easy navigation for users. As we transferred these blueprints into 
+                        HTML and CSS, each element mirrored the client's creativity.</p>
+                      <a href="https://github.com/mariaelenacossio/beautybyamy.github.io" class="more hover link-yellow">Github Repository</a>
+                    </div>
+                    <!-- /.post-content -->
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+              <!-- /.project-details -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.project -->
+          <div class="project item">
+            <div class="row">
+              <figure class="col-lg-9 offset-lg-3 col-xl-7 offset-xl-5 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/11/traffic-cone.png" alt="" /></figure>
+              <div class="project-details d-flex justify-content-center flex-column ms-lg-n150 ms-xl-0" style="left: 18%; bottom: 25%;">
+                <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
+                  <div class="card-body">
+                    <div class="post-header">
+                      <div class="post-category text-line text-green mb-3">Testing and Iteration</div>
+                      <h2 class="post-title mb-3">Pixel-Perfect Polishing</h2>
+                    </div>
+                    <!-- /.post-header -->
+                    <div class="post-content">
+                      <p>Our HTML and CSS coding underwent meticulous scrutiny to ensure 
+                        compatibility across browsers and devices. User testing refined 
+                        the website's responsiveness and design aesthetics.</p>
+                      <!-- <a href="#" class="more hover link-green">See Project</a> -->
+                    </div>
+                    <!-- /.post-content -->
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+              <!-- /.project-details -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.project -->
+          <div class="project item">
+            <div class="row">
+              <figure class="col-lg-8 col-xl-6 offset-xl-1 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/11/traffic-cone.png" alt="" /></figure>
+              <div class="project-details d-flex justify-content-center flex-column" style="right: 15%; bottom: 25%;">
+                <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
+                  <div class="card-body">
+                    <div class="post-header">
+                      <div class="post-category text-line text-red mb-3">Conclusion</div>
+                      <h2 class="post-title mb-3">Nailing Success, One Click at a Time</h2>
+                    </div>
+                    <!-- /.post-header -->
+                    <div class="post-content">
+                      <p>With a custom-made website coded in HTML and CSS, our nail tech is now flourishing in the digital realm. 
+                        Her online presence attracts clients, setting the stage for a flourishing career and the dream of owning 
+                        her own salon, all hosted on GitHub for the world to admire.</p>
+                      <a href="https://mariaelenacossio.github.io/beautybyamy.github.io/" class="more hover link-red">See Project</a>
+                    </div>
+                    <!-- /.post-content -->
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+              <!-- /.project-details -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.project -->
+        </div>
+        <!-- /.projects-overflow -->
+      </div>
+      <!-- /.container -->
+    </section>
+    <!-- /section -->
+    <?php
+get_footer();?>
+  </div>
+  <!-- /.content-wrapper -->
+  
+
+</div><!--.div interface-->
+
+</body>
+
+</html>
+
