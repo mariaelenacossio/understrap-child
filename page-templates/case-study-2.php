135fbd30478742d1cfd688ebef846084abae97f2
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-02 02:17:41 -0700

    Add info for case study

---
+++ page-templates/case-study-2.php
@@ -0,0 +1,230 @@
+<?php
+/**
+ * Template Name: Case Study Page (2) Template
+ *
+ * Template for displaying a blank page.
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+    <?php
+get_header();
+?>
+</head>
+<body>
+
+<div class="interface">
+    <section class="wrapper bg-light">
+      <div class="container pt-10 pt-md-14 text-center">
+        <div class="row">
+          <div class="col-md-8 col-lg-7 col-xl-6 col-xxl-5 mx-auto">
+            <h1 class="display-1 mb-3">Case Study</h1>
+            <p class="lead fs-lg px-lg-10 px-xxl-8">Local E-commerce Website</p>
+          </div>
+          <!-- /column -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </section>
+    <!-- /section -->
+    <section class="wrapper bg-light">
+      <div class="container pt-9 pt-md-11 pb-14 pb-md-16">
+        <div class="projects-overflow mt-md-10 mb-10 mb-lg-15">
+          <div class="project item">
+            <div class="row">
+              <figure class="col-lg-8 col-xl-7 offset-xl-1 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/10/website-project-1.png" alt="" /></figure>
+              <div class="project-details d-flex justify-content-center flex-column" style="right: 10%; bottom: 25%;">
+                <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
+                  <div class="card-body">
+                    <div class="post-header">
+                      <div class="post-category text-line text-purple mb-3">Introduction</div>
+                      <h2 class="post-title mb-3">Los Quequitos MX</h2>
+                    </div>
+                    <!-- /.post-header -->
+                    <div class="post-content">
+                      <p>A beloved local gem, sought to extend its charm online. 
+                        This journey began by defining the project scope and understanding 
+                        the bakery's unique selling points.</p>
+                      <!-- <a href="#" class="more hover link-purple">See Project</a> -->
+                    </div>
+                    <!-- /.post-content -->
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+              <!-- /.project-details -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.project -->
+          <div class="project item">
+            <div class="row">
+              <figure class="col-lg-7 offset-lg-5 col-xl-6 offset-xl-5 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/11/traffic-cone.png" alt="" /></figure>
+              <div class="project-details d-flex justify-content-center flex-column" style="left: 18%; bottom: 25%;">
+                <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
+                  <div class="card-body">
+                    <div class="post-header">
+                      <div class="post-category text-line text-leaf mb-3">Research and Discovery</div>
+                      <h2 class="post-title mb-3">Understanding Local Taste Preferences</h2>
+                    </div>
+                    <!-- /.post-header -->
+                    <div class="post-content">
+                      <p>I conducted thorough market research to identify competitors and understand local tastes. 
+                        This step allowed us to pinpoint the key features necessary for the website.</p>
+                      <!-- <a href="#" class="more hover link-leaf">See Project</a> -->
+                    </div>
+                    <!-- /.post-content -->
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+              <!-- /.project-details -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.project -->
+          <div class="project item">
+            <div class="row">
+              <figure class="col-lg-9 col-xl-7 offset-xl-2 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/11/traffic-cone.png" alt="" /></figure>
+              <div class="project-details d-flex justify-content-center flex-column" style="right: 3%; bottom: 25%;">
+                <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
+                  <div class="card-body">
+                    <div class="post-header">
+                      <div class="post-category text-line text-violet mb-3">Ideation and Design</div>
+                      <h2 class="post-title mb-3">From Recipes to Pixels: Crafting a Digital Identity</h2>
+                    </div>
+                    <!-- /.post-header -->
+                    <div class="post-content">
+                      <p>This phase was all about translating the bakery's delightful treats into a captivating visual identity. 
+                        Using HTML, CSS, and Bootstrap, we designed a site that mirrored the bakery's warmth and coziness. 
+                        The color palette was carefully selected to evoke feelings of comfort and joy, while the typography maintained readability and elegance.</p>
+                      <a href="https://xd.adobe.com/view/c382503b-c117-4a2c-b978-6a2a01edd463-96d1/" class="more hover link-violet">Xd Wireframes</a>
+                    </div>
+                    <!-- /.post-content -->
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+              <!-- /.project-details -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.project -->
+          <div class="project item">
+            <div class="row">
+              <figure class="col-lg-9 offset-lg-3 col-xl-7 offset-xl-4 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/11/traffic-cone.png" alt="" /></figure>
+              <div class="project-details d-flex justify-content-center flex-column" style="left: 12%; bottom: 25%;">
+                <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
+                  <div class="card-body">
+                    <div class="post-header">
+                      <div class="post-category text-line text-yellow mb-3">Prototyping</div>
+                      <h2 class="post-title mb-3">From Dough to Code: Baking a Delicious Website</h2>
+                    </div>
+                    <!-- /.post-header -->
+                    <div class="post-content">
+                      <p>With the design in place, we delved into the practical aspect. 
+                        We structured the website using HTML for content, CSS for styling, 
+                        and Bootstrap to ensure responsiveness across various devices. 
+                        The brand colors and logo were incorporated cohesively.</p>
+                      <a href="https://github.com/mariaelenacossio/losquequitosmx.github.io" class="more hover link-yellow">Github Repository</a>
+                    </div>
+                    <!-- /.post-content -->
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+              <!-- /.project-details -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.project -->
+          <div class="project item">
+            <div class="row">
+              <figure class="col-lg-9 offset-lg-3 col-xl-7 offset-xl-5 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/11/traffic-cone.png" alt="" /></figure>
+              <div class="project-details d-flex justify-content-center flex-column ms-lg-n150 ms-xl-0" style="left: 18%; bottom: 25%;">
+                <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
+                  <div class="card-body">
+                    <div class="post-header">
+                      <div class="post-category text-line text-green mb-3">Testing and Iteration</div>
+                      <h2 class="post-title mb-3">Tasting Success: Feedback and Refinement</h2>
+                    </div>
+                    <!-- /.post-header -->
+                    <div class="post-content">
+                      <p>Quality assurance was key. We ran extensive tests to ensure the website was fully functional, 
+                        visually appealing, and easy to navigate. User feedback played a significant role, 
+                        allowing us to fine-tune the site for an optimal user experience.</p>
+                      <!-- <a href="#" class="more hover link-green">See Project</a> -->
+                    </div>
+                    <!-- /.post-content -->
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+              <!-- /.project-details -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.project -->
+          <div class="project item">
+            <div class="row">
+              <figure class="col-lg-8 col-xl-6 offset-xl-1 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/11/traffic-cone.png" alt="" /></figure>
+              <div class="project-details d-flex justify-content-center flex-column" style="right: 15%; bottom: 25%;">
+                <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
+                  <div class="card-body">
+                    <div class="post-header">
+                      <div class="post-category text-line text-red mb-3">Conclusion</div>
+                      <h2 class="post-title mb-3">Serving Up Success: A Sweet New Online Presence</h2>
+                    </div>
+                    <!-- /.post-header -->
+                    <div class="post-content">
+                      <p>The culmination of our efforts resulted in a mouthwatering website that now serves as a digital 
+                        extension of the business. Customized to meet market standards, this site is not only 
+                        visually engaging but also intuitive for users, ensuring the bakery continues to sweeten its customer 
+                        relationships.</p>
+                      <a href="https://mariaelenacossio.github.io/losquequitosmx.github.io/" class="more hover link-red">See Project</a>
+                    </div>
+                    <!-- /.post-content -->
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+              <!-- /.project-details -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.project -->
+        </div>
+        <!-- /.projects-overflow -->
+      </div>
+      <!-- /.container -->
+    </section>
+    <!-- /section -->
+    <?php
+get_footer();?>
+  </div>
+  <!-- /.content-wrapper -->
+  
+
+</div><!--.div interface-->
+
+</body>
+
+</html>
