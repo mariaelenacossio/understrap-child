40c0b1509964a6159b9cd3bfd874010c5f2a29ca
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-01 23:58:39 -0700

    Remove changes and add logo

--- home.php
+++ home.php
@@ -281,7 +281,7 @@ $container = get_theme_mod( 'understrap_container_type' );
             <!--Grid column for image-->
             <div class="col-md-6 mb-4">
             <div class="contact-image text-center bg-primary rounded p-3">
-            <img class="img-fluid rounded" loading="lazy" src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/10/0D5A5721-1403-4809-9FFD-E66184DEB02E_1_102_o-e1697088508893.jpeg" alt="">
+            <img class="img-fluid rounded" loading="lazy" src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/10/logo-white-bg-black.png" alt="">
             </div>
             </div>
 
