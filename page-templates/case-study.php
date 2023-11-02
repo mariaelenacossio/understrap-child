1e2df1568562a912bcde0a9d2d90d52b1c55ee4e
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-01 20:23:40 -0700

    Remove changes

--- page-templates/case-study.php
+++ page-templates/case-study.php
@@ -24,55 +24,7 @@ defined( 'ABSPATH' ) || exit;
 get_header();
 ?>
 <div class="interface">
-<div class="container mt-5">
-        <!-- Introduction Section -->
-        <section class="mb-5">
-            <h2 class="display-4">Introduction</h2>
-            <p>Your introduction text goes here...</p>
-        </section>
 
-        <!-- Presentation Overview Section -->
-        <section class="mb-5">
-            <h2 class="display-4">Presentation Overview</h2>
-            <p>Your presentation overview text goes here...</p>
-        </section>
-
-        <!-- Case Study Format Section -->
-        <section class="mb-5">
-            <h2 class="display-4">Case Study Format</h2>
-            <!-- Case Study Step 1: Define the problem -->
-            <div class="mb-4">
-                <h3>1. Define the Problem</h3>
-                <p>Your description of the problem and how you addressed it goes here...</p>
-            </div>
-            <!-- Case Study Step 2: Design process and methodology -->
-            <div class="mb-4">
-                <h3>2. Design Process and Methodology</h3>
-                <p>Your description of your design process and methodology goes here...</p>
-            </div>
-            <!-- Case Study Step 3: Key insights and user research -->
-            <div class="mb-4">
-                <h3>3. Key Insights and User Research</h3>
-                <p>Your key findings from user research and how they influenced your design decisions go here...</p>
-            </div>
-            <!-- Case Study Step 4: User testing and iteration -->
-            <div class="mb-4">
-                <h3>4. User Testing and Iteration</h3>
-                <p>Your process of gathering feedback and iterative improvements based on user insights goes here...</p>
-            </div>
-            <!-- Case Study Step 5: Results and impact -->
-            <div class="mb-4">
-                <h3>5. Results and Impact</h3>
-                <p>Your results, positive outcomes, metrics, challenges faced, solutions, and lessons learned go here...</p>
-            </div>
-        </section>
-
-        <!-- Key Takeaways and Design Principles Section -->
-        <section class="mb-5">
-            <h2 class="display-4">Key Takeaways and Design Principles</h2>
-            <p>Your key takeaways and design principles description goes here...</p>
-        </section>
-    </div>
 	<?php
 get_footer();?>
 </div><!--.div interface-->
