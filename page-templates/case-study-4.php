a446ab1262368d520be5e17c3e80b672fa2485d4
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-02 03:06:51 -0700

    Add info and img for case study

---
+++ page-templates/case-study-4.php
@@ -0,0 +1,231 @@
+<?php
+/**
+ * Template Name: Case Study Page (4) Template
+ *
+ * Template for displaying a blank page.
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+    <?php
+get_header();
+?>
+</head>
+<body>
+
+<div class="interface">
+    <section class="wrapper bg-light">
+      <div class="container pt-10 pt-md-14 text-center">
+        <div class="row">
+          <div class="col-md-8 col-lg-7 col-xl-6 col-xxl-5 mx-auto">
+            <h1 class="display-1 mb-3">Case Study</h1>
+            <p class="lead fs-lg px-lg-10 px-xxl-8">Web App/Landing Page</p>
+          </div>
+          <!-- /column -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </section>
+    <!-- /section -->
+    <section class="wrapper bg-light">
+      <div class="container pt-9 pt-md-11 pb-14 pb-md-16">
+        <div class="projects-overflow mt-md-10 mb-10 mb-lg-15">
+          <div class="project item">
+            <div class="row">
+              <figure class="col-lg-8 col-xl-7 offset-xl-1 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/10/website-project-4.png" alt="" /></figure>
+              <div class="project-details d-flex justify-content-center flex-column" style="right: 10%; bottom: 25%;">
+                <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
+                  <div class="card-body">
+                    <div class="post-header">
+                      <div class="post-category text-line text-purple mb-3">Introduction</div>
+                      <h2 class="post-title mb-3">relocateME</h2>
+                    </div>
+                    <!-- /.post-header -->
+                    <div class="post-content">
+                      <p>For this college project, I had to create an accommodation app, 
+                        which seemed simple at first but held more complexity beneath the surface. 
+                        It involved understanding not just design principles but also user psychology, 
+                        technical feasibility, and the nuances of the accommodation industry.</p>
+                      <!-- <a href="#" class="more hover link-purple">See Project</a> -->
+                    </div>
+                    <!-- /.post-content -->
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+              <!-- /.project-details -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.project -->
+          <div class="project item">
+            <div class="row">
+              <figure class="col-lg-7 offset-lg-5 col-xl-6 offset-xl-5 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/11/User-Persona.png" alt="" /></figure>
+              <div class="project-details d-flex justify-content-center flex-column" style="left: 18%; bottom: 25%;">
+                <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
+                  <div class="card-body">
+                    <div class="post-header">
+                      <div class="post-category text-line text-leaf mb-3">Research and Discovery</div>
+                      <h2 class="post-title mb-3">Finding the Perfect Fit</h2>
+                    </div>
+                    <!-- /.post-header -->
+                    <div class="post-content">
+                      <p>Extensive research became the cornerstone. I analyzed market trends, surveyed potential users, 
+                        and identified their pain points. Crafting detailed user personas was crucial to understand their 
+                        diverse needs. Task flows were mapped out to ensure that the app could serve as a one-stop solution.</p>
+                      <!-- <a href="#" class="more hover link-leaf">See Project</a> -->
+                    </div>
+                    <!-- /.post-content -->
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+              <!-- /.project-details -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.project -->
+          <div class="project item">
+            <div class="row">
+              <figure class="col-lg-9 col-xl-7 offset-xl-2 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/11/wireframe-landing-page-1.png" alt="" /></figure>
+              <div class="project-details d-flex justify-content-center flex-column" style="right: 3%; bottom: 25%;">
+                <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
+                  <div class="card-body">
+                    <div class="post-header">
+                      <div class="post-category text-line text-violet mb-3">Ideation and Design</div>
+                      <h2 class="post-title mb-3">Brushing on the Creativity</h2>
+                    </div>
+                    <!-- /.post-header -->
+                    <div class="post-content">
+                      <p>Ideas were brainstormed, and wireframes were drafted. The landing page design was meticulously created, 
+                        keeping user experience and branding in focus. This stage demanded striking a balance between creativity 
+                        and practicality.</p>
+                      <a href="#!" class="more hover link-violet">Wireframes</a>
+                    </div>
+                    <!-- /.post-content -->
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+              <!-- /.project-details -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.project -->
+          <div class="project item">
+            <div class="row">
+              <figure class="col-lg-9 offset-lg-3 col-xl-7 offset-xl-4 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/11/traffic-cone.png" alt="" /></figure>
+              <div class="project-details d-flex justify-content-center flex-column" style="left: 12%; bottom: 25%;">
+                <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
+                  <div class="card-body">
+                    <div class="post-header">
+                      <div class="post-category text-line text-yellow mb-3">Prototyping</div>
+                      <h2 class="post-title mb-3">From Concept to Clickable</h2>
+                    </div>
+                    <!-- /.post-header -->
+                    <div class="post-content">
+                      <p>The transformation from static designs to a clickable prototype was intriguing. 
+                        The complexity lay in making sure that every interaction felt intuitive, ensuring a seamless experience. 
+                        Adobe XD proved invaluable in bringing our ideas to life.</p>
+                      <a href="https://xd.adobe.com/view/750b48dd-e710-43ea-9533-4016c88f3f66-2058/" class="more hover link-yellow">Adobe Xd Prototype</a>
+                    </div>
+                    <!-- /.post-content -->
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+              <!-- /.project-details -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.project -->
+          <div class="project item">
+            <div class="row">
+              <figure class="col-lg-9 offset-lg-3 col-xl-7 offset-xl-5 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/11/task-flow-relocate-ME.png" alt="" /></figure>
+              <div class="project-details d-flex justify-content-center flex-column ms-lg-n150 ms-xl-0" style="left: 18%; bottom: 25%;">
+                <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
+                  <div class="card-body">
+                    <div class="post-header">
+                      <div class="post-category text-line text-green mb-3">Testing and Iteration</div>
+                      <h2 class="post-title mb-3">Perfecting the Polish</h2>
+                    </div>
+                    <!-- /.post-header -->
+                    <div class="post-content">
+                      <p>User testing was where the real adventure began. Complex scenarios and real-time user 
+                        feedback highlighted areas for improvement. It was a continuous process of refinement, 
+                        with iterations becoming more polished, one after another.</p>
+                      <!-- <a href="#" class="more hover link-green">See Project</a> -->
+                    </div>
+                    <!-- /.post-content -->
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+              <!-- /.project-details -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.project -->
+          <div class="project item">
+            <div class="row">
+              <figure class="col-lg-8 col-xl-6 offset-xl-1 rounded"> <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/11/traffic-cone.png" alt="" /></figure>
+              <div class="project-details d-flex justify-content-center flex-column" style="right: 15%; bottom: 25%;">
+                <div class="card shadow rellax" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
+                  <div class="card-body">
+                    <div class="post-header">
+                      <div class="post-category text-line text-red mb-3">Conclusion</div>
+                      <h2 class="post-title mb-3">Polished and Ready to Shine</h2>
+                    </div>
+                    <!-- /.post-header -->
+                    <div class="post-content">
+                      <p>The project culminated in an app that not only met the initial assignment but surpassed it. 
+                        It had evolved from a mere concept to a fully functional, user-centered solution. 
+                        From complexities to simplicities, it was a journey of learning, innovation, and perseverance.</p>
+                      <a href="https://xd.adobe.com/view/ef8d82b4-5695-4dfa-847f-2f7955ed59ef-bade/" class="more hover link-red">See Project Landing Page</a>
+                    </div>
+                    <!-- /.post-content -->
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+              <!-- /.project-details -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.project -->
+        </div>
+        <!-- /.projects-overflow -->
+      </div>
+      <!-- /.container -->
+    </section>
+    <!-- /section -->
+    <?php
+get_footer();?>
+  </div>
+  <!-- /.content-wrapper -->
+  
+
+</div><!--.div interface-->
+
+</body>
+
+</html>
+
