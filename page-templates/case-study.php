f35389035259660b10af4ea719ab2e311e560a52
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-01 20:49:31 -0700

    Remove changes

--- page-templates/case-study.php
+++ page-templates/case-study.php
@@ -17,7 +17,6 @@ defined( 'ABSPATH' ) || exit;
 	<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-    <script src="./js/plugins.js"></script>
 	<?php wp_head(); ?>
 </head>
 <body>
