dee04a8c9be42f43acbaa05e3bf8f600b36ef9b5
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-05 20:23:55 -0800

    Add function to form submission and email

--- mail.php
+++ mail.php
@@ -1,34 +1,45 @@
 <?php
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
-  $subject = $_POST['subject'];
-  header('Content-Type: application/json');
-  if ($name === '') {
-    print json_encode(array('message' => 'Name cannot be empty', 'code' => 0));
-    exit();
-  }
-  if ($email === '') {
-    print json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
-    exit();
-  } else {
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      print json_encode(array('message' => 'Email format invalid.', 'code' => 0));
-      exit();
+// Check if the form was submitted
+if (isset($_POST['submit'])) {
+    // Get the form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    // Recipient email (website owner's email)
+    $to = 'mariaelena.cossio@outlook.com';
+
+    // Create the email headers
+    $headers = "From: $email" . "\r\n" .
+        "Reply-To: $email" . "\r\n" .
+        "X-Mailer: PHP/" . phpversion();
+
+    // Compose the email message to the owner
+    $email_message_owner = "Name: $name\n";
+    $email_message_owner .= "Email: $email\n";
+    $email_message_owner .= "Subject: $subject\n";
+    $email_message_owner .= "Message:\n$message";
+
+    // Send the email to the owner
+    $success_owner = mail($to, "Contact Form Submission: $subject", $email_message_owner, $headers);
+
+    if ($success_owner) {
+        // Send a confirmation email to the user
+        $user_email = $email; // User's email
+        $confirmation_message = "Thank you for contacting us. We will get back to you soon.";
+        $success_user = mail($user_email, "Confirmation: $subject", $confirmation_message, $headers);
+        
+        // Display a success message
+        if ($success_user) {
+            echo 'Emails sent successfully!';
+        } else {
+            echo 'Confirmation email could not be sent.';
+        }
+    } else {
+        echo 'Email to website owner could not be sent.';
     }
-  }
-  if ($subject === '') {
-    print json_encode(array('message' => 'Subject cannot be empty', 'code' => 0));
-    exit();
-  }
-  if ($message === '') {
-    print json_encode(array('message' => 'Message cannot be empty', 'code' => 0));
-    exit();
-  }
-  $content = "From: $name \nEmail: $email \nMessage: $message";
-  $recipient = "mariaelena.cossio@outlook.com";
-  $mailheader = "From: $email \r\n";
-  mail($recipient, $subject, $content, $mailheader) or die("Error!");
-  print json_encode(array('message' => 'Email successfully sent!', 'code' => 1));
-  exit();
+} else {
+    echo 'Form was not submitted.';
+}
 ?>
