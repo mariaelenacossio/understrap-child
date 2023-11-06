851cb44734b276c8b25f84584c98097590aeb86b
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-05 21:39:29 -0800

    Adjust file for contact form

--- mail.php
+++ mail.php
@@ -1,40 +1,59 @@
 <?php
-// Include WordPress functions, including wp_mail()
-define('WP_USE_THEMES', false);
-require('wp-mail.php');
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
+require 'vendor/autoload.php'; // Include the PHPMailer library
+
+// Check if the form was submitted
 if (isset($_POST['submit'])) {
-    $name = sanitize_text_field($_POST['name']);
-    $email = sanitize_email($_POST['email']);
-    $subject = sanitize_text_field($_POST['subject']);
-    $message = esc_textarea($_POST['message']);
+    // Get the form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-    // Recipient email (website owner's email)
-    $to = 'mariaelena.cossio@outlook.com';
+    // Initialize PHPMailer
+    $mail = new PHPMailer(true);
 
-    // Compose the email headers
-    $headers = "Content-Type: text/html\r\n";
-    $headers .= "From: $name <$email>\r\n";
+    try {
+        // Set up the email server settings for Outlook.com
+        $mail->isSMTP();
+        $mail->Host = 'smtp-mail.outlook.com'; // SMTP server for Outlook.com
+        $mail->SMTPAuth = true;
+        $mail->Username = 'mariaelena.cossio@outlook.com'; // Your Outlook.com email address
+        $mail->Password = 'Hassive1mar2.'; // Your Outlook.com password
+        $mail->SMTPSecure = 'tls'; // Use TLS encryption
+        $mail->Port = 587; // Port for Outlook.com SMTP
 
-    // Call the custom wp_mail() function
-    $success_owner = custom_wp_mail($to, "Contact Form Submission: $subject", $message, $headers);
+        // Set sender and recipient addresses
+        $mail->setFrom('your-outlook-email@example.com', 'Your Name'); // Your Outlook.com email and name
+        $mail->addAddress('mariaelena.cossio@outlook.com', 'Website Owner'); // Owner's email
 
-    if ($success_owner) {
-        // Send a confirmation email to the user
-        $user_email = $email; // User's email
-        $confirmation_message = "Thank you for contacting us. We will get back to you soon.";
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = 'Contact Form Submission: ' . $subject;
+        $mail->Body = "Name: $name<br>Email: $email<br>Message: $message";
 
-        // Call the custom wp_mail() function for the user confirmation email
-        $success_user = custom_wp_mail($user_email, "Confirmation: $subject", $confirmation_message, $headers);
+        // Send the email to the owner
+        $mail->send();
+
+        // Send a confirmation email to the person who filled out the form
+        $mail->clearAddresses();
+        $mail->addAddress($email, $name); // User's email
+        $mail->Subject = 'Confirmation: ' . $subject;
+        $mail->Body = "Thank you for contacting us. We will get back to you soon.";
+
+        // Send the confirmation email
+        $mail->send();
 
         // Display a success message
-        if ($success_user) {
-            echo 'Emails sent successfully!';
-        } else {
-            echo 'Confirmation email could not be sent.';
-        }
-    } else {
-        echo 'Email to website owner could not be sent.';
+        echo 'Emails sent successfully!';
+    } catch (Exception $e) {
+        // Handle any errors that occur during sending
+        echo 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo;
     }
+} else {
+    // Handle cases where the form was not submitted
+    echo 'Form was not submitted.';
 }
 ?>
