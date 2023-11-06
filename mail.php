50f8df2935ba2750a7d3ca90cba903c3aec0c14d
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-05 20:32:07 -0800

    Format function for wp_mail()

--- mail.php
+++ mail.php
@@ -1,35 +1,33 @@
 <?php
-// Check if the form was submitted
 if (isset($_POST['submit'])) {
-    // Get the form data
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+    $name = sanitize_text_field($_POST['name']);
+    $email = sanitize_email($_POST['email']);
+    $subject = sanitize_text_field($_POST['subject']);
+    $message = esc_textarea($_POST['message']);
 
     // Recipient email (website owner's email)
     $to = 'mariaelena.cossio@outlook.com';
 
-    // Create the email headers
-    $headers = "From: $email" . "\r\n" .
-        "Reply-To: $email" . "\r\n" .
-        "X-Mailer: PHP/" . phpversion();
+    // Compose the email headers
+    $headers = "Content-Type: text/html\r\n";
+    $headers .= "From: $name <$email>\r\n";
 
     // Compose the email message to the owner
-    $email_message_owner = "Name: $name\n";
-    $email_message_owner .= "Email: $email\n";
-    $email_message_owner .= "Subject: $subject\n";
-    $email_message_owner .= "Message:\n$message";
+    $email_message_owner = "Name: $name<br>";
+    $email_message_owner .= "Email: $email<br>";
+    $email_message_owner .= "Subject: $subject<br>";
+    $email_message_owner .= "Message:<br>$message";
 
     // Send the email to the owner
-    $success_owner = mail($to, "Contact Form Submission: $subject", $email_message_owner, $headers);
+    $success_owner = wp_mail($to, "Contact Form Submission: $subject", $email_message_owner, $headers);
 
     if ($success_owner) {
         // Send a confirmation email to the user
         $user_email = $email; // User's email
         $confirmation_message = "Thank you for contacting us. We will get back to you soon.";
-        $success_user = mail($user_email, "Confirmation: $subject", $confirmation_message, $headers);
-        
+
+        $success_user = wp_mail($user_email, "Confirmation: $subject", $confirmation_message, $headers);
+
         // Display a success message
         if ($success_user) {
             echo 'Emails sent successfully!';
