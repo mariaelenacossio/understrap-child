b1986d5e5f9ab2bf205566360200df8014207fba
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-05 22:40:33 -0800

    Change author post name and icon

--- page-templates/blog-post-1.php
+++ page-templates/blog-post-1.php
@@ -36,8 +36,8 @@ get_header();
               <!-- /.post-category -->
               <h1 class="display-1 mb-4">Commodo Dolor Bibendum Parturient Cursus Mollis</h1>
               <ul class="post-meta mb-5">
-                <li class="post-date"><i class="uil uil-calendar-alt"></i><span>5 Jul 2022</span></li>
-                <li class="post-author"><a href="#"><i class="uil uil-user"></i><span>By Sandbox</span></a></li>
+                <li class="post-date"><i class="uil uil-calendar-alt"></i><span>Nov 5 2023</span></li>
+                <li class="post-author"><a href="#"><i class="fa-regular fa-calendar"></i><span>Mariaelena C</span></a></li>
                 <li class="post-comments"><a href="#"><i class="uil uil-comment"></i>3<span> Comments</span></a></li>
                 <li class="post-likes"><a href="#"><i class="uil uil-heart-alt"></i>3<span> Likes</span></a></li>
               </ul>
