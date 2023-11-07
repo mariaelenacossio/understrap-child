598ba0cfc343c0e16203efbd440db7897297d3c4
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-06 22:51:38 -0800

    Change text color for categories

--- page-templates/blog-post-1.php
+++ page-templates/blog-post-1.php
@@ -37,7 +37,7 @@ get_header();
     <div class="col-md-6">
       <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-primary">Inclusive Design</strong><!--Category-->
+          <strong class="d-inline-block mb-2 text-line text-purple">Inclusive Design</strong><!--Category-->
           <h3 class="mb-0">Designing for All</h3>
           <div class="mb-1 text-muted">Nov 06</div>
           <p class="card-text mb-auto">How to create interfaces that accommodate users with disabilities, fostering inclusivity in design.</p>
@@ -52,7 +52,7 @@ get_header();
     <div class="col-md-6">
       <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-success">Design</strong>
+          <strong class="d-inline-block mb-2 text-success">Design</strong><!--Category-->
           <h3 class="mb-0">Post title</h3>
           <div class="mb-1 text-muted">Nov 11</div>
           <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
