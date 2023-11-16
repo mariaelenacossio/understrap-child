6ee6043c58fb8c623b306d77a3fde20232d6df2c
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-16 00:19:54 -0800

    Add content for one card below

--- page-templates/blog-post-1.php
+++ page-templates/blog-post-1.php
@@ -138,14 +138,18 @@ get_header();
       <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div class="col p-4 d-flex flex-column position-static">
           <strong class="d-inline-block mb-2 text-line text-purple">Inclusive Design</strong><!--Category-->
-          <h3 class="mb-0">Designing for All</h3>
+          <h3 class="mb-0">Color Psychology in Web Design: A Palette for Emotions</h3>
           <div class="mb-1 text-muted">Nov 06</div>
-          <p class="card-text mb-auto">How to create interfaces that accommodate users with disabilities, fostering inclusivity in design.</p>
-          <a href="#" class="stretched-link">Continue reading</a>
+          <p class="card-text mb-auto">Intrigued? Let's uncover the magic of Color Psychology in Web Design – where every shade tells a story!.</p>
+          <a href="#blog-post-2" class="stretched-link">Continue reading</a>
         </div>
         <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="https://mariaelena-cossioclark.com/wp-content/uploads/2023/11/blog-post-2-scaled.jpg" 
+          role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+          <title>Placeholder</title>
+          <rect width="100%" height="100%" fill="#55595c"/>
+          <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+        </svg>
         </div>
       </div>
     </div>
