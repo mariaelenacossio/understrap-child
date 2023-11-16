0ceffe8d6dedd71627b493e18c0dba3382e2397c
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-16 01:50:13 -0800

    Update blog post 2

---
+++ page-templates/blog-post-2.php
@@ -0,0 +1,190 @@
+<?php
+/**
+ * Template Name: Blog Post (2) Template
+ *
+ * Template for displaying a blank page.
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+    <?php
+get_header();
+?>
+</head>
+<body>
+
+<div class="interface">
+<main class="container">
+<div class="p-4 p-md-5 mb-4 rounded bg-with-image">
+    <div class="col-md-12 px-0">
+        <h1 class="display-1 text-primary text-center">Color Psychology in Web Design</h1>
+        <p class="lead text-center my-3">A Palette for Emotions</p>
+    </div>
+</div>
+
+
+
+  <div class="row g-5">
+    <div class="col-md-12">
+      <article class="blog-post">
+        <!-- <h2 class="blog-post-title display-1 text-center">Mobile-First Design</h2>
+        <p class="lead my-3 text-center">Optimizing UX for Smartphone Users</p> -->
+        <p class="blog-post-meta text-end">November 11th, 2023 by <a href="#">Mariaelena C.</a></p>
+
+        <p class="p">Welcome, color enthusiasts! 🌈 Today, we're diving deep into the world of hues, 
+          shades, and vibes. Imagine your website as a canvas, and each color as a brushstroke painting 
+          the emotions of your users. Intrigued? Let's uncover the magic of Color Psychology in Web Design 
+          – where every shade tells a story!</p>
+        
+          <div class="headings">
+				<h2 id="blog-post-1" class="display-4 text-center pt-5">
+          <span class="text-gradient d-inline">Why Color Matters?</span></h2>
+				<hr>
+				</div>
+        <p class="p">
+        Colors aren't just pretty pixels; they're emotional wizards. Cool blues can 
+        create a sense of trust and tranquility, while fiery reds ignite passion and urgency. 
+        The palette you choose sets the tone for your digital space, influencing how users feel 
+        and interact with your site.
+        </p>
+
+        <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-6">
+            <h3 id="#!" class="display-6 text-center pt-5">
+              <span class="text-gradient d-inline">Crafting Your Emotion Palette</span></h3>
+                <p class="p">
+                It's time to become a digital emotion maestro! We'll guide you through the art 
+                of selecting colors that resonate with your brand and connect with your audience. 
+                From the comforting embrace of greens to the invigorating power of yellows, we'll 
+                help you paint a picture that truly speaks to your users.
+                </p>
+            </div><!--.col-md-6-->
+            <div class="col-md-6">
+                <img src="https://mariaelena-cossioclark.com/wp-content/uploads/2023/11/blog-post-img-1-scaled.jpg" 
+                alt="responsive design, user experience, mobile-friendly ui, interaction design, 
+                navigation patterns, app prototyping, app prototyping, design trends" 
+                class="img-fluid" loading="eager">
+            </div><!--.col-md-6-->
+        </div><!--.row-->
+    </div><!--.container mt-5-->
+
+    <div class="container mt-5">
+        <div class="row">
+        <div class="col-md-6">
+                <img src="https://mariaelena-cossioclark.com/wp-content/uploads/2023/11/blog-post-img-2-scaled.jpg" 
+                alt="responsive design, user experience, mobile-friendly ui, interaction design, 
+                navigation patterns, app prototyping, app prototyping, design trends" 
+                class="img-fluid" loading="eager">
+            </div><!--.col-md-6-->
+            <div class="col-md-6">
+            <h3 id="#!" class="display-6 text-center pt-5">
+              <span class="text-gradient d-inline">A Rainbow of Possibilities</span></h3>
+                <p class="p">
+                We're not just talking about red, blue, and green; we're talking about the 
+                entire rainbow of possibilities! Discover the emotional nuances of each color 
+                and how to combine them to create a symphony of feelings on your website. 
+                Get ready to unleash the power of your personalized color spectrum.
+                </p>
+        </div><!--.row-->
+    </div><!--.container mt-5-->
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+            <h3 id="#!" class="display-6 text-center pt-5">
+              <span class="text-gradient d-inline">Let's Play with Palettes!</span></h3>
+                <p class="p">
+                Enough theory – it's time to play! We'll explore real-world examples of 
+                websites that have nailed color psychology. Get inspired by the color 
+                choices of industry leaders and see how they've used the emotional language 
+                of colors to captivate their audience.
+                </p>
+                <h4 class="display-6 text-center pt-5">Your Palette, Your Story</h4>
+                <p class="p">
+                As you embark on this colorful journey, remember: your color palette is more 
+                than a design choice; it's a storyteller. What story do you want your website 
+                to tell? What emotions do you want to evoke? It's time to infuse your digital 
+                canvas with personality and warmth.
+                </p>
+                <p class="p">
+                Ready to paint your website with emotions? 
+                Let's dive in and create a palette that leaves a lasting 
+                impression on your users. Get ready for a rainbow ride of creativity 
+                and connection! 🚀🎨
+                <br>
+                Happy scrolling! 📱✨
+                </p>
+            </div><!--.col-md-6-->
+      </div><!--.row-->
+      </div><!--.container mt-5-->
+
+      <nav class="blog-pagination" aria-label="Pagination">
+        <a class="btn btn-outline-primary" href="#">Older</a>
+        <a class="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">Newer</a>
+      </nav>
+
+
+  <div class="row mb-2">
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-line text-purple">Mobile Design</strong><!--Category-->
+          <h3 class="mb-0">Crafting an Enjoyable Mobile Experience</h3>
+          <div class="mb-1 text-muted">Nov 06</div>
+          <p class="card-text mb-auto">Let's embark on a journey of discovery as we unravel the secrets 
+                            behind optimizing user experiences for our beloved smartphone users.</p>
+          <a href="#/post-1" class="stretched-link">Continue reading</a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" 
+               xmlns="http://www.w3.org/2000/svg" role="img" 
+               aria-label="Placeholder: Thumbnail" 
+               preserveAspectRatio="xMidYMid slice" focusable="false">
+               <title>Placeholder</title>
+               <rect width="100%" height="100%" fill="#55595c"/>
+               <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+              </svg>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-success">Design</strong><!--Category-->
+          <h3 class="mb-0">Navigating the UX Seas</h3>
+          <div class="mb-1 text-muted">Nov 11</div>
+          <p class="mb-auto">Smooth navigation is the wind in their sails, guiding them effortlessly through your digital waters. .</p>
+          <a href="#" class="stretched-link">Continue reading</a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+        </div>
+      </div>
+    </div>
+  </div>  
+
+</main>
+<?php
+get_footer();
+?>
+  
+
+</div><!--.div interface-->
+
+</body>
+
+</html>
+
