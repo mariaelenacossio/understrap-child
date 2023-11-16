d215b99184765ebdbd935434fe890285e7d023de
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-16 00:45:47 -0800

    Remove changes

--- page-templates/blog-post-1.php
+++ page-templates/blog-post-1.php
@@ -133,25 +133,29 @@ get_header();
       </nav>
 
 
-      <div class="row mb-2">
+  <div class="row mb-2">
     <div class="col-md-6">
-        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div class="col-md-6 p-4 d-flex flex-column position-static">
-                <strong class="d-inline-block mb-2 text-line text-purple">Inclusive Design</strong><!--Category-->
-                <h3 class="mb-0">Color Psychology in Web Design: A Palette for Emotions</h3>
-                <div class="mb-1 text-muted">Nov 06</div>
-                <p class="card-text mb-auto">Intrigued? Let's uncover the magic of Color Psychology in Web Design – where every shade tells a story!.</p>
-                <a href="#blog-post-2" class="stretched-link">Continue reading</a>
-            </div>
-            <div class="col-md-6 d-none d-lg-block">
-                <img class="img-fluid" src="https://mariaelena-cossioclark.com/wp-content/uploads/2023/11/blog-post-2-scaled.jpg"
-                    alt="responsive design, user experience, mobile-friendly ui, interaction design, 
-                    navigation patterns, app prototyping, app prototyping, design trends" 
-                    loading="eager" />
-            </div>
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-line text-purple">Inclusive Design</strong><!--Category-->
+          <h3 class="mb-0">Color Psychology in Web Design: A Palette for Emotions</h3>
+          <div class="mb-1 text-muted">Nov 06</div>
+          <p class="card-text mb-auto">Intrigued? Let's uncover the magic of Color Psychology 
+                    in Web Design – where every shade tells a story!.</p>
+          <a href="#" class="stretched-link">Continue reading</a>
         </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" 
+               xmlns="http://www.w3.org/2000/svg" role="img" 
+               aria-label="Placeholder: Thumbnail" 
+               preserveAspectRatio="xMidYMid slice" focusable="false">
+               <title>Placeholder</title>
+               <rect width="100%" height="100%" fill="#55595c"/>
+               <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+              </svg>
+        </div>
+      </div>
     </div>
-</div>
     <div class="col-md-6">
       <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div class="col p-4 d-flex flex-column position-static">
