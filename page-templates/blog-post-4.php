8217dd97a208a350d41f2d0af9716d887281e0e6
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-16 02:18:28 -0800

    Add content for blog post 4

---
+++ page-templates/blog-post-4.php
@@ -0,0 +1,198 @@
+<?php
+/**
+ * Template Name: Blog Post (4) Template
+ *
+ * Template for displaying a blank page.
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+    <?php
+get_header();
+?>
+</head>
+<body>
+
+<div class="interface">
+<main class="container">
+<div class="p-4 p-md-5 mb-4 rounded bg-with-image">
+    <div class="col-md-12 px-0">
+        <h1 class="display-1 text-primary text-center">Microinteractions</h1>
+        <p class="lead text-center my-3">The Small Details That Make a Big Impact</p>
+    </div>
+</div>
+
+
+
+  <div class="row g-5">
+    <div class="col-md-12">
+      <article class="blog-post">
+        <!-- <h2 class="blog-post-title display-1 text-center">Mobile-First Design</h2>
+        <p class="lead my-3 text-center">Optimizing UX for Smartphone Users</p> -->
+        <p class="blog-post-meta text-end">November 13th, 2023 by <a href="#">Mariaelena C.</a></p>
+
+        <p class="p">Greetings, design aficionados! 🎨✨ Today, we're delving into the enchanting 
+          world of microinteractions – those tiny details that pack a punch and add a sprinkle 
+          of magic to your website. Join us on a journey where small becomes mighty, and every 
+          click, hover, and scroll turns into a delightful experience for your users.</p>
+        
+          <div class="headings">
+				<h2 id="blog-post-1" class="display-4 text-center pt-5">
+          <span class="text-gradient d-inline">What Are Microinteractions, Anyway?</span></h2>
+				<hr>
+				</div>
+        <p class="p">
+        Think of microinteractions as the subtle gestures and responses that bring your
+        website to life. It's the gentle bounce of a button after a click, the soft 
+        glow when hovering over an icon, or the satisfying 'ping' when a form is 
+        successfully submitted. These small, thoughtful details create a connection 
+        between your users and your website, turning mundane interactions into moments 
+        of delight.
+        </p>
+
+        <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-6">
+            <h3 id="#!" class="display-6 text-center pt-5">
+              <span class="text-gradient d-inline">Why Do Microinteractions Matter?</span></h3>
+                <p class="p">
+                The devil is in the details, they say, and microinteractions are the angels 
+                that make your user experience heavenly. We'll explore how these small 
+                details enhance usability, provide feedback, and add that extra dash of 
+                personality to your digital space. Users might not consciously notice them, 
+                but trust us, they'll feel the difference.
+                </p>
+            </div><!--.col-md-6-->
+            <div class="col-md-6">
+                <img src="https://mariaelena-cossioclark.com/wp-content/uploads/2023/11/blog-post-img-1-scaled.jpg" 
+                alt="responsive design, user experience, mobile-friendly ui, interaction design, 
+                navigation patterns, app prototyping, app prototyping, design trends" 
+                class="img-fluid" loading="eager">
+            </div><!--.col-md-6-->
+        </div><!--.row-->
+    </div><!--.container mt-5-->
+
+    <div class="container mt-5">
+        <div class="row">
+        <div class="col-md-6">
+                <img src="https://mariaelena-cossioclark.com/wp-content/uploads/2023/11/blog-post-img-2-scaled.jpg" 
+                alt="responsive design, user experience, mobile-friendly ui, interaction design, 
+                navigation patterns, app prototyping, app prototyping, design trends" 
+                class="img-fluid" loading="eager">
+            </div><!--.col-md-6-->
+            <div class="col-md-6">
+            <h3 id="#!" class="display-6 text-center pt-5">
+              <span class="text-gradient d-inline">Crafting Delightful Micro-Moments</span></h3>
+                <p class="p">
+                Get ready to roll up your sleeves and infuse your website with personality! 
+                We'll share tips and tricks on how to identify opportunities for 
+                microinteractions and guide you through the process of designing these 
+                delightful micro-moments. From the initial hover to the final click, 
+                we'll help you create a symphony of interactions that leave a lasting 
+                impression.
+                </p>
+        </div><!--.row-->
+    </div><!--.container mt-5-->
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+            <h3 id="#!" class="display-6 text-center pt-5">
+              <span class="text-gradient d-inline">Real-Life Examples of Micro-Magic</span></h3>
+                <p class="p">
+                Let's draw inspiration from the masters of micro-magic! We'll 
+                showcase websites that have nailed the art of thoughtful 
+                microinteractions, turning ordinary tasks into extraordinary 
+                experiences. These examples will spark your creativity and show 
+                you the endless possibilities of adding micro-moments to your design 
+                repertoire.
+                </p>
+                <h4 class="display-6 text-center pt-5">It's the Little Things That Count</h4>
+                <p class="p">
+                As we wrap up this exploration, remember – it's the little things 
+                that count. Whether it's a subtle animation, a clever transition, 
+                or a tiny sound effect, these microinteractions contribute to the 
+                overall joy of interacting with your website. Get ready to dive 
+                into the world of micro-magic and create an experience that's not 
+                just functional but downright delightful. Let's make those micro-moments 
+                count! 🚀🌟
+                </p>
+                <br>
+                Happy scrolling! 📱✨
+                </p>
+            </div><!--.col-md-6-->
+      </div><!--.row-->
+      </div><!--.container mt-5-->
+
+      <nav class="blog-pagination" aria-label="Pagination">
+        <a class="btn btn-outline-primary" href="#">Older</a>
+        <a class="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">Newer</a>
+      </nav>
+
+
+  <div class="row mb-2">
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-line text-purple">Mobile Design</strong><!--Category-->
+          <h3 class="mb-0">Navigating the UX Seas</h3>
+          <div class="mb-1 text-muted">Nov 10</div>
+          <p class="card-text mb-auto">In this blog post, we're not just talking 
+            about menus and buttons – we're navigating the UX seas to create 
+            an experience that feels like a breezy cruise.</p>
+          <a href="https://mariaelena-cossioclark.com/blog-post-3/" class="stretched-link">Continue reading</a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" 
+               xmlns="http://www.w3.org/2000/svg" role="img" 
+               aria-label="Placeholder: Thumbnail" 
+               preserveAspectRatio="xMidYMid slice" focusable="false">
+               <title>Placeholder</title>
+               <rect width="100%" height="100%" fill="#55595c"/>
+               <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+              </svg>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-success">Design</strong><!--Category-->
+          <h3 class="mb-0">Embrace the Future</h3>
+          <div class="mb-1 text-muted">Nov 06</div>
+          <p class="mb-auto">Let's embark on a journey of discovery 
+            as we unravel the secrets behind optimizing user experiences 
+            for our beloved smartphone users.</p>
+          <a href="https://mariaelena-cossioclark.com/blog-post-1/" class="stretched-link">Continue reading</a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+        </div>
+      </div>
+    </div>
+  </div>  
+
+</main>
+<?php
+get_footer();
+?>
+  
+
+</div><!--.div interface-->
+
+</body>
+
+</html>
+
