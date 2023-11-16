dabc05a1628198066217da6ba05074e03c83e20b
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-16 01:05:32 -0800

    Update second card

--- page-templates/blog-post-1.php
+++ page-templates/blog-post-1.php
@@ -160,9 +160,9 @@ get_header();
       <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div class="col p-4 d-flex flex-column position-static">
           <strong class="d-inline-block mb-2 text-success">Design</strong><!--Category-->
-          <h3 class="mb-0">Post title</h3>
+          <h3 class="mb-0">Navigating the UX Seas</h3>
           <div class="mb-1 text-muted">Nov 11</div>
-          <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+          <p class="mb-auto">Smooth navigation is the wind in their sails, guiding them effortlessly through your digital waters. .</p>
           <a href="#" class="stretched-link">Continue reading</a>
         </div>
         <div class="col-auto d-none d-lg-block">
