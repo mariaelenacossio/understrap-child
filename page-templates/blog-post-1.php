28e1eb087fac2ed87138292d04be0b2e5a37d951
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-20 12:48:14 -0800

    Test for blog post cards

--- page-templates/blog-post-1.php
+++ page-templates/blog-post-1.php
@@ -171,7 +171,30 @@ get_header();
         </div>
       </div>
     </div>
-  </div>  
+  </div>
+  
+  <!--Test for a card-->
+  <div class="box">
+      <div class="group">
+        <div class="overlap-group">
+          <div class="rectangle"></div>
+          <div class="div"></div>
+          <img class="lightbulb" src="img/lightbulb-1.png" />
+          <div class="rectangle-2"></div>
+          <div class="text-wrapper">Step 1</div>
+          <p class="lorem-ipsum-is">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+            industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
+            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+            Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+          <div class="text-wrapper-2">Title</div>
+        </div>
+      </div>
+    </div>
+
 
 </main>
 <?php
