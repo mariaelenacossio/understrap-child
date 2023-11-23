fe76ed6ccd74fb16602ee5565fb613aa486247f2
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-23 01:27:02 -0700

    Remove previous layout

--- page-templates/blog-post-1.php
+++ page-templates/blog-post-1.php
@@ -25,14 +25,6 @@ get_header();
 <body>
 
 <div class="interface">
-<main class="container">
-<div class="p-4 p-md-5 mb-4 rounded bg-with-image">
-    <div class="col-md-12 px-0">
-        <h1 class="display-1 text-primary text-center">Crafting an Enjoyable Mobile Experience</h1>
-        <p class="lead text-center my-3">Unlocking the Delight of Mobile-First Design for Smoother User Journeys</p>
-    </div>
-</div>
-
 <!--Test for new layout-->
 <div class="container-blog mt-4">
   <div class="row">
@@ -135,130 +127,7 @@ get_header();
 </div>
 
 
-  <div class="row g-5">
-    <div class="col-md-12">
-      <article class="blog-post">
-        <!-- <h2 class="blog-post-title display-1 text-center">Mobile-First Design</h2>
-        <p class="lead my-3 text-center">Optimizing UX for Smartphone Users</p> -->
-        <p class="blog-post-meta text-end">November 6th, 2023 by <a href="#">Mariaelena C.</a></p>
-
-        <p class="p">Hey there, tech enthusiasts! 🚀 Welcome to our digital realm where we're 
-          diving headfirst into the vibrant world of Mobile-First Design. Today, let's embark
-          on a journey of discovery as we unravel the secrets behind optimizing user experiences 
-          for our beloved smartphone users.</p>
-        
-          <div class="headings">
-				<h2 id="blog-post-1" class="display-4 text-center pt-5">
-          <span class="text-gradient d-inline">Why Mobile-First?</span></h2>
-				<hr>
-				</div>
-
-<!--Test-->
-<div class="container-blog">
-  <div class="content-blog">
-    <div class="row">
-      <div class="col-lg-6">
-        <div class="section-title">
-          <div class="content-2">
-            <p class="heading">Because It's All About You!</p>
-            <p class="p">
-                In a world where our smartphones are practically an extension of ourselves, 
-                it's time to ensure that the digital landscape caters to your needs seamlessly. 
-                Mobile-First Design isn't just a trend; it's a revolution that puts you at the 
-                heart of the digital experience. We're talking about a design philosophy that 
-                prioritizes the small screen but packs a punch when it comes to delivering user 
-                satisfaction.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-6">
-        <img class="placeholder-image" src="https://mariaelena-cossioclark.com/wp-content/uploads/2023/11/blog-post-img-1-scaled.jpg" 
-                alt="responsive design, user experience, mobile-friendly ui, interaction design, 
-                navigation patterns, app prototyping, app prototyping, design trends">
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
-
-
-        <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-6">
-            <h3 id="#!" class="display-6 text-center pt-5">
-              <span class="text-gradient d-inline">Because It's All About You!</span></h3>
-                <p class="p">
-                In a world where our smartphones are practically an extension of ourselves, 
-                it's time to ensure that the digital landscape caters to your needs seamlessly. 
-                Mobile-First Design isn't just a trend; it's a revolution that puts you at the 
-                heart of the digital experience. We're talking about a design philosophy that 
-                prioritizes the small screen but packs a punch when it comes to delivering user 
-                satisfaction.
-                </p>
-            </div><!--.col-md-6-->
-            <div class="col-md-6">
-                <img src="https://mariaelena-cossioclark.com/wp-content/uploads/2023/11/blog-post-img-1-scaled.jpg" 
-                alt="responsive design, user experience, mobile-friendly ui, interaction design, 
-                navigation patterns, app prototyping, app prototyping, design trends" 
-                class="img-fluid" loading="eager">
-            </div><!--.col-md-6-->
-        </div><!--.row-->
-    </div><!--.container mt-5-->
-
-    <div class="container mt-5">
-        <div class="row">
-        <div class="col-md-6">
-                <img src="https://mariaelena-cossioclark.com/wp-content/uploads/2023/11/blog-post-img-2-scaled.jpg" 
-                alt="responsive design, user experience, mobile-friendly ui, interaction design, 
-                navigation patterns, app prototyping, app prototyping, design trends" 
-                class="img-fluid" loading="eager">
-            </div><!--.col-md-6-->
-            <div class="col-md-6">
-            <h3 id="#!" class="display-6 text-center pt-5">
-              <span class="text-gradient d-inline">Say Goodbye to Pinch and Zoom, Hello to Smooth Sailing!</span></h3>
-                <p class="p">
-                Remember those frustrating days of constantly pinching and zooming just to read a webpage on your phone?
-                Well, say goodbye to that headache! Mobile-First Design is all about breaking free from the constraints 
-                of traditional design, offering you an effortlessly fluid and enjoyable navigation experience.
-                </p>
-        </div><!--.row-->
-    </div><!--.container mt-5-->
-
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-12">
-            <h3 id="#!" class="display-6 text-center pt-5">
-              <span class="text-gradient d-inline">Fun, Friendly, and Fast – The Triple 'F' Mantra</span></h3>
-                <p class="p">
-                Picture this: You open a website on your smartphone, and it feels like you're on a joy ride. 
-                That's the magic of our Mobile-First approach – it's not just functional; it's downright fun! 
-                Friendly interfaces, intuitive gestures, and lightning-fast loading times are the cornerstones 
-                of a design that puts a smile on your face.
-                </p>
-                <h4 class="display-6 text-center pt-5">It's All About You – The Smartphone Superhero!</h4>
-                <p class="p">
-                Your smartphone is not just a device; it's a superhero in your pocket, ready to rescue you from boredom, 
-                connect you with loved ones, and entertain you on the go. Mobile-First Design pays homage to this superhero
-                by ensuring that every digital interaction is a delightful experience, whether you're checking out the 
-                latest news, shopping for goodies, or simply exploring the vastness of the internet.
-                </p>
-                <h5 class="display-6 text-center pt-5">The Journey Begins – Let's Dive In!</h5>
-                <p class="p">
-                Ready to explore the wonders of Mobile-First Design together? Buckle up, 
-                fellow smartphone enthusiasts, as we take a joyous ride into a realm where user experience reigns supreme, 
-                and your satisfaction is our top priority. It's time to embrace the future, where mobile is not just first 
-                but fabulous!.
-                So, what are you waiting for? Let's make your smartphone experience the best it can be – because you deserve it!
-                <br>
-                Happy scrolling! 📱✨
-                </p>
-            </div><!--.col-md-6-->
-      </div><!--.row-->
-      </div><!--.container mt-5-->
-
+  
       <nav class="blog-pagination" aria-label="Pagination">
         <a class="btn btn-outline-primary" href="#">Older</a>
         <a class="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">Newer</a>
