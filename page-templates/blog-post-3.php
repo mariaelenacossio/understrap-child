3d6c221b1281a45219989ada3af68589ae983190
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-11-23 02:47:33 -0700

    Add new layout

--- page-templates/blog-post-3.php
+++ page-templates/blog-post-3.php
@@ -25,160 +25,142 @@ get_header();
 <body>
 
 <div class="interface">
-<main class="container">
-<div class="p-4 p-md-5 mb-4 rounded bg-with-image">
-    <div class="col-md-12 px-0">
-        <h1 class="display-1 text-primary text-center">Navigating the UX Seas</h1>
-        <p class="lead text-center my-3">A Guide to Intuitive Navigation Design</p>
-    </div>
+<!--Test for new layout-->
+<div class="container-blog mt-4">
+  <div class="row">
+    <!-- First Column (Scrollable) -->
+    <div class="col-lg-8 first-column">
+    <div class="banner-container">
+      <img src="https://mariaelena-cossioclark.com/wp-content/uploads/2023/11/blog-post-img-1-scaled.jpg" 
+                alt="responsive design, user experience, mobile-friendly ui, interaction design, 
+                navigation patterns, app prototyping, app prototyping, design trends" class="banner-img">
 </div>
+                <div class="article-details">
+        <p class="text-muted mb-0"><span class="font-weight-bold">Nov 12, 2023</span></p>
+        <p class="text-muted mb-0"><span class="font-weight-bold">5 minute read</span></p>
+        <p class="text-muted mb-0">-<span class="font-weight-bold">Mariaelena C</span></p>
+      </div>      
+      <h1 class="text-center pt-4">Navigating the UX Seas</h1>
+      <p class="lead my-3">The Ocean of User Experience</p>
+      <p class="p">Ahoy, digital sailors! 🌊 Ready to set sail on the high seas of user experience 
+                  design? Great navigation is your compass, and in this post, we're turning you into a captain 
+                  of smooth sailing. Get ready to chart your course through the UX seas, ensuring your users 
+                  have a journey as smooth as gliding on calm waters.</p>
 
+          <!-- <p class="lead my-3">Why Mobile-First?</p> -->
+          <!-- <p class="text-gradient d-inline">Because It's All About You!</p> -->
+             <p class="p">Think of your website as a vast ocean, and your users as fearless sailors seeking 
+                          information and experiences. Smooth navigation is the wind in their sails, guiding
+                          them effortlessly through your digital waters. In this blog post, we're not just 
+                          talking about menus and buttons – we're navigating the UX seas to create an 
+                          experience that feels like a breezy cruise.</p>
 
+      <span class="text-gradient d-inline">Crafting Intuitive Navigation Maps</span></h3>
+              <p class="p">What good is a sea voyage without a map? We're handing you the compass 
+                            to intuitive navigation design. Learn the art of creating menus that 
+                            are not just functional but feel like a friendly guide leading your users. 
+                            Dive into the world of user-friendly labels, responsive layouts, 
+                            and a navigation system that adapts to any device – because a smooth 
+                            sea journey is meant for all sailors, big and small.
+                            </p>
 
-  <div class="row g-5">
-    <div class="col-md-12">
-      <article class="blog-post">
-        <!-- <h2 class="blog-post-title display-1 text-center">Mobile-First Design</h2>
-        <p class="lead my-3 text-center">Optimizing UX for Smartphone Users</p> -->
-        <p class="blog-post-meta text-end">November 12th, 2023 by <a href="#">Mariaelena C.</a></p>
-
-        <p class="p">Ahoy, digital sailors! 🌊 Ready to set sail on the high seas of user experience 
-          design? Great navigation is your compass, and in this post, we're turning you into a captain 
-          of smooth sailing. Get ready to chart your course through the UX seas, ensuring your users 
-          have a journey as smooth as gliding on calm waters.</p>
-        
-          <div class="headings">
-				<h2 id="blog-post-1" class="display-4 text-center pt-5">
-          <span class="text-gradient d-inline">The Ocean of User Experience</span></h2>
-				<hr>
-				</div>
-        <p class="p">
-        Think of your website as a vast ocean, and your users as fearless sailors seeking 
-        information and experiences. Smooth navigation is the wind in their sails, guiding
-        them effortlessly through your digital waters. In this blog post, we're not just 
-        talking about menus and buttons – we're navigating the UX seas to create an 
-        experience that feels like a breezy cruise.
-        </p>
-
-        <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-6">
-            <h3 id="#!" class="display-6 text-center pt-5">
-              <span class="text-gradient d-inline">Crafting Intuitive Navigation Maps</span></h3>
-                <p class="p">
-                What good is a sea voyage without a map? We're handing you the compass 
-                to intuitive navigation design. Learn the art of creating menus that 
-                are not just functional but feel like a friendly guide leading your users. 
-                Dive into the world of user-friendly labels, responsive layouts, 
-                and a navigation system that adapts to any device – because a smooth 
-                sea journey is meant for all sailors, big and small.
+      <span class="text-gradient d-inline">Avoiding the Storms of Confusion</span></h3>
+                <p class="p">Ever been on a website that felt like a turbulent storm, leaving you lost and 
+                            disoriented? Let's steer clear of that! We're sharing tips on how to avoid the 
+                            common pitfalls that create confusion for your users. From clear signposts to 
+                            straightforward calls-to-action, we're turning you into a weathered captain, 
+                            steering your ship through calm waters.
                 </p>
-            </div><!--.col-md-6-->
-            <div class="col-md-6">
-                <img src="https://mariaelena-cossioclark.com/wp-content/uploads/2023/11/blog-post-img-1-scaled.jpg" 
-                alt="responsive design, user experience, mobile-friendly ui, interaction design, 
-                navigation patterns, app prototyping, app prototyping, design trends" 
-                class="img-fluid" loading="eager">
-            </div><!--.col-md-6-->
-        </div><!--.row-->
-    </div><!--.container mt-5-->
 
-    <div class="container mt-5">
-        <div class="row">
-        <div class="col-md-6">
-                <img src="https://mariaelena-cossioclark.com/wp-content/uploads/2023/11/blog-post-img-2-scaled.jpg" 
-                alt="responsive design, user experience, mobile-friendly ui, interaction design, 
-                navigation patterns, app prototyping, app prototyping, design trends" 
-                class="img-fluid" loading="eager">
-            </div><!--.col-md-6-->
-            <div class="col-md-6">
-            <h3 id="#!" class="display-6 text-center pt-5">
-              <span class="text-gradient d-inline">Avoiding the Storms of Confusion</span></h3>
-                <p class="p">
-                Ever been on a website that felt like a turbulent storm, leaving you lost and 
-                disoriented? Let's steer clear of that! We're sharing tips on how to avoid the 
-                common pitfalls that create confusion for your users. From clear signposts to 
-                straightforward calls-to-action, we're turning you into a weathered captain, 
-                steering your ship through calm waters.
+                <h4 class="display-6 text-center pt-5">Smooth Waters Ahead!</h4>
+                <p class="p">With the wind at your back and the stars as your guide, you'll sail 
+                              into the horizon of user satisfaction. We'll showcase real-life 
+                              examples of websites that have mastered the art of intuitive navigation, 
+                              providing you with inspiration to captain your own digital ship.
                 </p>
-        </div><!--.row-->
-    </div><!--.container mt-5-->
 
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-12">
-            <h3 id="#!" class="display-6 text-center pt-5">
-              <span class="text-gradient d-inline">Smooth Waters Ahead!**</span></h3>
-                <p class="p">
-                With the wind at your back and the stars as your guide, you'll sail 
-                into the horizon of user satisfaction. We'll showcase real-life 
-                examples of websites that have mastered the art of intuitive navigation, 
-                providing you with inspiration to captain your own digital ship.
-                </p>
-                <h4 class="display-6 text-center pt-5">Chart Your Course to Success</h4>
-                <p class="p">
-                Ready to embark on this nautical adventure? It's time to chart your 
-                course to success with navigation that not only gets your users from 
-                point A to B but does so with the grace of a well-tuned sailboat. 
-                Navigate with purpose, and let's set sail towards the horizon of 
-                seamless user experiences. Anchors aweigh! 🌊⚓
-                </p>
+                <h5 class="display-6 text-center pt-5">Chart Your Course to Success</h5>
+                <p class="p">Ready to embark on this nautical adventure? It's time to chart your 
+                            course to success with navigation that not only gets your users from 
+                            point A to B but does so with the grace of a well-tuned sailboat. 
+                            Navigate with purpose, and let's set sail towards the horizon of 
+                            seamless user experiences. Anchors aweigh! 🌊⚓
                 <br>
                 Happy scrolling! 📱✨
                 </p>
-            </div><!--.col-md-6-->
-      </div><!--.row-->
-      </div><!--.container mt-5-->
 
+
+    </div>
+
+    <!-- Second Column (Static) -->
+    <div class="col-lg-4 second-column">
+      <!-- Display cards side by side on screens smaller than 1024px -->
+      <div class="d-md-flex flex-md-row flex-wrap">
+      <!-- First Card -->
+      <!--Blog post 1-->
+      <div class="card card-blog-post">
+      <a href="https://mariaelena-cossioclark.com/blog-post-1/">
+        <img src="https://mariaelena-cossioclark.com/wp-content/uploads/2023/11/blog-post-img-1-scaled.jpg" 
+                alt="responsive design, user experience, mobile-friendly ui, interaction design, 
+                navigation patterns, app prototyping, app prototyping, design trends" class="card-img-top card-img-blog-post">
+        </a>
+        <div class="card-body">
+        <div class="article-details">
+            <p class="card-text text-muted mb-0"><span class="font-weight-bold">Nov 06, 2023</span></p>
+            <p class="card-text text-muted mb-0"><span class="font-weight-bold">5 minutes read</span></p>
+            <p class="card-text text-muted mb-0">-<span class="font-weight-bold">Mariaelena C</span></p>
+          </div>          
+          <h5 class="card-title">Crafting an Enjoyable Mobile Experience</h5>
+          <p class="card-text">Explore Mobile-First Design's vibrant world. 
+                              Embark on a journey optimizing user experiences for smartphone users.</p>
+        </div>
+      </div>
+
+      <!-- Second Card -->
+      <!--Blog post 2-->
+      <div class="card card-blog-post">
+      <a href="https://mariaelena-cossioclark.com/blog-post-2-2/">
+        <img src="http://mariaelena-cossioclark.com/wp-content/uploads/2023/11/blog-post-2-scaled.jpg" 
+                alt="responsive design, user experience, mobile-friendly ui, interaction design, 
+                navigation patterns, app prototyping, app prototyping, design trends" class="card-img-top card-img-blog-post">
+      </a>
+        <div class="card-body">
+        <div class="article-details">
+            <p class="card-text text-muted mb-0"><span class="font-weight-bold">Nov 11, 2023</span></p>
+            <p class="card-text text-muted mb-0"><span class="font-weight-bold">5 minutes read</span></p>
+            <p class="card-text text-muted mb-0">-<span class="font-weight-bold">Mariaelena C</span></p>
+          </div>          
+          <h5 class="card-title">Color Psychology in Web Design</h5>
+          <p class="card-text">Explore the vivid realm of color in web design, 
+                              where hues and shades craft user emotions. Dive in!</p>
+        </div>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+  
       <nav class="blog-pagination" aria-label="Pagination">
-        <a class="btn btn-outline-primary" href="#">Older</a>
-        <a class="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">Newer</a>
+        <a class="btn btn-outline-primary" href="https://mariaelena-cossioclark.com/blog-post-3/">Older</a>
+        <a class="btn btn-outline-secondary" href="https://mariaelena-cossioclark.com/blog-post-2-2/" tabindex="-1">Newer</a>
       </nav>
 
 
-  <div class="row mb-2">
-    <div class="col-md-6">
-      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-line text-purple">Mobile Design</strong><!--Category-->
-          <h3 class="mb-0">Color Psychology in Web Design</h3>
-          <div class="mb-1 text-muted">Nov 11</div>
-          <p class="card-text mb-auto">Intrigued? Let's uncover the magic of 
-            Color Psychology in Web Design – where every shade tells a story!</p>
-          <a href="https://mariaelena-cossioclark.com/blog-post-2-2/" class="stretched-link">Continue reading</a>
-        </div>
-        <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" 
-               xmlns="http://www.w3.org/2000/svg" role="img" 
-               aria-label="Placeholder: Thumbnail" 
-               preserveAspectRatio="xMidYMid slice" focusable="false">
-               <title>Placeholder</title>
-               <rect width="100%" height="100%" fill="#55595c"/>
-               <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-              </svg>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-6">
-      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-success">Design</strong><!--Category-->
-          <h3 class="mb-0">Microinteractions: The Small Details</h3>
-          <div class="mb-1 text-muted">Nov 13</div>
-          <p class="mb-auto">Join us on a journey where small becomes mighty, 
-            and every click, hover, and scroll turns into a delightful experience 
-            for your users.</p>
-          <a href="https://mariaelena-cossioclark.com/blog-post-4/" class="stretched-link">Continue reading</a>
-        </div>
-        <div class="col-auto d-none d-lg-block">
-          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
 
-        </div>
-      </div>
-    </div>
-  </div>  
 
-</main>
+
+
+
+
+
+
+
+
+
+
 <?php
 get_footer();
 ?>
