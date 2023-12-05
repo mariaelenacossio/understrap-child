e6df1d5eebc33f48d002c5c530a6d0924a2a2fb4
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-12-04 22:15:29 -0800

    Add animation for hero section

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -2,13 +2,24 @@
 "use strict";
 jQuery.noConflict();
 
-//active state for side-bar btn
+//Active state for side-bar btn
 jQuery(document).ready(function($) {
     $(".nav-item").click(function() {
         // Remove active class from all nav-items
         $(".nav-item").removeClass("active");
         // Add active class to the clicked nav-item
         $(this).addClass("active");
+    });
+});
+
+//Add animation for hero section
+jQuery(document).ready(function($) {
+    var container = $(".container.hero");
+
+    // Wait for the image to load
+    $(".img-fluid-hero").on('load', function() {
+        // Add the 'show' class to the container
+        container.addClass('show');
     });
 });
 
