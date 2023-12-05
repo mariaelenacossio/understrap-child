1e810dfd3bd7bbe367895cad353f7471148e8468
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-12-05 02:05:28 -0800

    Update hero img bg

--- home.php
+++ home.php
@@ -25,7 +25,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="parallax-container">
     <div class="parallax-layer">
-        <img src="https://mariaelena-cossioclark.com/wp-content/uploads/2023/12/hero-img-5.png" 
+        <img src="https://mariaelena-cossioclark.com/wp-content/uploads/2023/12/hero-img-4.png" 
             alt="Web Design" class="parallax-image">
         <div class="parallax-overlay hidden">
             <div class="container h-100">
