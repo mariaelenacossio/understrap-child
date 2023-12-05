3d1018adcc073db50af9658350205719a1e1efde
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-12-04 22:57:15 -0800

    Add function for typing role animation

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -31,21 +31,45 @@ jQuery(document).ready(function($) {
     }, 900); // Adjust the delay according to your needs
 });
 
-//Parallax effect for hero section
+//Parallax effect for hero section and typing script for the roles
 jQuery(document).ready(function($) {
-    var parallaxContainer = $(".parallax-container");
-
-    $(window).scroll(function() {
-        var scrollPos = $(this).scrollTop();
-        var translateY = -scrollPos * 0.3;
-
-        parallaxContainer.find('.parallax-image').css("transform", "translateY(" + translateY + "px)");
+        var roleText = $(".role");
+        var roles = ["Front-end Developer", "Content Creator", "UX/UI Designer"];
+        var currentRoleIndex = 0;
+    
+        function typeRole(role, index, callback) {
+            var typedRole = "";
+            var roleInterval = setInterval(function() {
+                typedRole += role[index];
+                roleText.text("Web Designer " + typedRole);
+                index++;
+                if (index >= role.length) {
+                    clearInterval(roleInterval);
+                    callback();
+                }
+            }, 100); // You can adjust the typing speed (milliseconds per character)
+        }
+    
+        function changeRole() {
+            roleText.fadeOut(400, function() {
+                typeRole(roles[currentRoleIndex], 0, function() {
+                    roleText.fadeIn(400, function() {
+                        // Increment the role index, and reset if it exceeds the number of roles
+                        currentRoleIndex = (currentRoleIndex + 1) % roles.length;
+                    });
+                });
+            });
+        }
+    
+        // Set an interval to change the role every 3000 milliseconds (3 seconds)
+        setInterval(changeRole, 3000);
+    
+        // Wait for the image to load
+        $(".parallax-image").on('load', function() {
+            // Add the 'show' class to the container
+            $(".parallax-container").addClass('show');
+        });
     });
-
-    // Wait for the image to load
-    $(".parallax-image").on('load', function() {
-        // Add the 'show' class to the container
-        parallaxContainer.addClass('show');
-    });
-});
+    
+    
 
