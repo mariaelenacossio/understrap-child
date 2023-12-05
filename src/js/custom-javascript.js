af41a7d27895b3096f7596536d7e15d6084f53e2
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-12-05 01:39:14 -0800

    Change value for mobile

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -85,7 +85,7 @@ jQuery(document).ready(function($) {
 //To hide the text when scrolling up
 jQuery(document).ready(function($) {
     var lastScrollTop = 0;
-    var mobileThreshold = 468; // Adjust this value based on your needs
+    var mobileThreshold = 900; // Adjust this value based on your needs
 
     $(window).scroll(function () {
         if ($(window).width() > mobileThreshold) {
