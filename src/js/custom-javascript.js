2ec1a8335035faa1537cf64be824dedb2b20b752
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-12-04 23:43:04 -0800

    Adjust function for typing effect

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -33,46 +33,80 @@ jQuery(document).ready(function($) {
 
 //Parallax effect for hero section and typing script for the roles
 jQuery(document).ready(function($) {
-    var roleText = $(".role");
-    var roles = ["Front-end Developer", "Content Creator", "UX/UI Designer"];
-    var currentRoleIndex = 0;
-
-    function typeRole(role, index, callback) {
-        var typedRole = "";
-        var roleInterval = setInterval(function() {
-            typedRole += role[index];
-            roleText.text("Web Designer " + typedRole);
-            index++;
-            if (index >= role.length) {
-                clearInterval(roleInterval);
-                callback();
-            }
-        }, 100); // You can adjust the typing speed (milliseconds per character)
-    }
-
-    function changeRole() {
-        typeRole("", 0, function() {
-            roleText.fadeOut(400, function() {
-                typeRole(roles[currentRoleIndex], 0, function() {
-                    roleText.fadeIn(400, function() {
-                        // Increment the role index, and reset if it exceeds the number of roles
-                        currentRoleIndex = (currentRoleIndex + 1) % roles.length;
+        var roleText = $("#roleText .role");
+        var cursorSpan = $("#roleText .cursor");
+    
+        var roles = ["Front-end Developer", "Content Creator", "UX/UI Designer"];
+        var currentRoleIndex = 0;
+    
+        function typeRole(role, index, callback) {
+            var typedRole = "";
+            var roleInterval = setInterval(function() {
+                typedRole += role[index];
+                roleText.text("Web Designer " + typedRole);
+                index++;
+                if (index >= role.length) {
+                    clearInterval(roleInterval);
+                    callback();
+                }
+            }, 100); // You can adjust the typing speed (milliseconds per character)
+        }
+    
+        function changeRole() {
+            typeRole("", 0, function() {
+                roleText.fadeOut(400, function() {
+                    typeRole(roles[currentRoleIndex], 0, function() {
+                        roleText.fadeIn(400, function() {
+                            // Increment the role index, and reset if it exceeds the number of roles
+                            currentRoleIndex = (currentRoleIndex + 1) % roles.length;
+                        });
                     });
                 });
             });
+        }
+    
+        // Set an interval to change the role every 3000 milliseconds (3 seconds)
+        setInterval(changeRole, 3000);
+    
+        // Text typing effect
+        const textArray = ["hard", "fun", "a journey", "LIFE"];
+        const typingDelay = 200;
+        const erasingDelay = 100;
+        const newTextDelay = 2000; // Delay between current and next text
+        let textArrayIndex = 0;
+        let charIndex = 0;
+    
+        function type() {
+            if (charIndex < textArray[textArrayIndex].length) {
+                if (!cursorSpan.hasClass("typing")) cursorSpan.addClass("typing");
+                roleText.text("Web Designer " + textArray[textArrayIndex].substring(0, charIndex + 1));
+                charIndex++;
+                setTimeout(type, typingDelay);
+            } else {
+                cursorSpan.removeClass("typing");
+                setTimeout(erase, newTextDelay);
+            }
+        }
+    
+        function erase() {
+            if (charIndex > 0) {
+                if (!cursorSpan.hasClass("typing")) cursorSpan.addClass("typing");
+                roleText.text("Web Designer " + textArray[textArrayIndex].substring(0, charIndex - 1));
+                charIndex--;
+                setTimeout(erase, erasingDelay);
+            } else {
+                cursorSpan.removeClass("typing");
+                textArrayIndex = (textArrayIndex + 1) % textArray.length;
+                setTimeout(type, typingDelay + 1100);
+            }
+        }
+    
+        // Initiate the typing effect on DOM Load
+        $(document).ready(function() {
+            if (textArray.length) setTimeout(type, newTextDelay + 250);
         });
-    }
-
-    // Set an interval to change the role every 3000 milliseconds (3 seconds)
-    setInterval(changeRole, 3000);
-
-    // Wait for the image to load
-    $(".parallax-image").on('load', function() {
-        // Add the 'show' class to the container
-        $(".parallax-container").addClass('show');
     });
-});
-
+    
     
     
 
