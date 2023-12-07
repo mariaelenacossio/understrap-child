bf2abae49e30f050b702d1e408a7bc2519a2d404
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-12-06 18:31:36 -0800

    Add function for page loader

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -134,6 +134,14 @@ jQuery(window).on('load', function() {
     }, 3000);
 });
 
+//Function for page loader
+const container = document.querySelector(".loader");
+for (let i = 1; i <= 100; i++) {
+  const circle = document.createElement("div");
+  circle.className = "circle";
+  circle.style.setProperty("--i", i);
+  container.appendChild(circle);
+}
 
 
 
