79c638a59b076daac985664fc4983106ac120e02
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-12-06 17:33:43 -0800

    Blog carousel test

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -134,6 +134,16 @@ jQuery(window).on('load', function() {
     }, 3000);
 });
 
+//Blog card carousel test
+const slider = document.querySelector(".slider");
+
+function activate(e) {
+  const items = document.querySelectorAll(".item");
+  e.target.matches(".next") && slider.append(items[0]);
+  e.target.matches(".prev") && slider.prepend(items[items.length - 1]);
+}
+
+document.addEventListener("click", activate, false);
 
 
 
