d6b6cc18211178e61562efbb26420f5b2d48bed8
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-12-06 17:05:18 -0800

    Remove class text-truncate

--- home.php
+++ home.php
@@ -139,7 +139,7 @@ $container = get_theme_mod( 'understrap_container_type' );
             <div>
               <!-- <p class="text-secondary mb-1">Nov 11, 2022</p> -->
               <h2 class="h1 mb-3"><a class="link-dark text-decoration-none" href="#!">Design System</a></h2>
-              <p class="mb-4 text-truncate" style="max-height: 3.6em; overflow: hidden;">Elevating User Experience with a Design System.</p>
+              <p class="mb-4" style="max-height: 3.6em; overflow: hidden;">Elevating User Experience with a Design System.</p>
 			  <div class="portfolio-btn mt-3"> <!-- This code will apply only for screens not bigger than 390px -->
 				<a class="btn btn-primary btn-lg px-4 me-md-2" href="https://mariaelena-cossioclark.com/beyond-skincare/" target="_self">Read More</a>
 				<a class="btn btn-outline-secondary btn-lg px-4 me-md-2" href="https://mariaelenacossio.github.io/DGL-309-design-system/" target="_blank">View Site</a>
@@ -193,7 +193,7 @@ $container = get_theme_mod( 'understrap_container_type' );
             <div>
               <!-- <p class="text-secondary mb-1">Sep 17, 2022</p> -->
               <h2 class="h1 mb-3"><a class="link-dark text-decoration-none" href="#!">Business/E-commerce</a></h2>
-              <p class="mb-4 text-truncate" style="max-height: 3.6em; overflow: hidden;">Crafting Elegance and User Experience with One Nail at a Time.</p>
+              <p class="mb-4r" style="max-height: 3.6em; overflow: hidden;">Crafting Elegance and User Experience with One Nail at a Time.</p>
               <div class="portfolio-btn mt-3 text-truncate" style="max-height: 3.6em; overflow: hidden;"> <!-- This code will apply only for screens not bigger than 390px -->
               <a class="btn btn-primary btn-lg px-4 me-md-2" href="https://mariaelena-cossioclark.com/beauty-by-amy/" target="_self">Read More</a>
 			  <a class="btn btn-outline-secondary btn-lg px-4 me-md-2" href="https://mariaelenacossio.github.io/beautybyamy.github.io/" target="_blank">View Site</a>
