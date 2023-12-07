ac72c8c77386710b49fcff2773c79f74bae4b058
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-12-06 18:38:06 -0800

    Remove changes

--- header.php
+++ header.php
@@ -24,8 +24,7 @@ $navbar_type       = get_theme_mod( 'understrap_navbar_type', 'collapse' );
 
 <body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
 <?php do_action( 'wp_body_open' ); ?>
-<!--Page loader-->
-<div class="loader"></div>
+
 <div class="site" id="page">
 
 	<!-- ******************* The Navbar Area ******************* -->
