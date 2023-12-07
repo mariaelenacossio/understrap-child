72d00685936767a706c3c0a513df7fdb045d502a
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-12-06 18:35:53 -0800

    Update page loader function

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -125,13 +125,13 @@ jQuery(document).ready(function($) {
 
 
 //Function for page loader
-const container = document.querySelector(".loader");
-for (let i = 1; i <= 100; i++) {
-  const circle = document.createElement("div");
-  circle.className = "circle";
-  circle.style.setProperty("--i", i);
-  container.appendChild(circle);
-}
+jQuery(document).ready(function($) {
+    const container = $(".loader");
+    for (let i = 1; i <= 100; i++) {
+      const circle = $("<div>").addClass("circle").css("--i", i);
+      container.append(circle);
+    }
+  });
 
 
 
