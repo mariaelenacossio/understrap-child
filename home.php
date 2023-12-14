03f7e52930cc013afc5849cda02e9252b477f3a8
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-12-13 22:03:05 -0800

    Adjust parallax hero

--- home.php
+++ home.php
@@ -51,10 +51,6 @@ $container = get_theme_mod( 'understrap_container_type' );
   </div><!--.window-container-->
 </div><!--.hero-container-->
 
-
-<!--Interface Content-->
-<div class="interface">
-
 <!--Parallax Container-Hero Section Mobile-->
 <div class="parallax-container">
     <div class="parallax-layer">
@@ -85,6 +81,12 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 
 		<!--End of Hero Section-->
+
+
+
+<!--Interface Content-->
+<div class="interface">
+
 
 <!-- About Me -->
 <section id="about" class="bg-light py-4 py-md-5 py-xl-6">
