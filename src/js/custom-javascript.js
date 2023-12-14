fa1c16add7b1f8cb5b509a20ba2bd66c4fc41800
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-12-14 15:08:44 -0800

    Add touch start event for typing effect

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -76,6 +76,16 @@ jQuery(document).ready(function($) {
     document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
       if(textArray.length) setTimeout(type, newTextDelay + 250);
     });    
+
+    // Add touchstart event listener in addition to mousedown
+    document.addEventListener("mousedown", startAnimation);
+    document.addEventListener("touchstart", startAnimation);
+
+    function startAnimation() {
+        if (charIndex === 0) {
+            type();
+        }
+    }
     
 //Parallax effect for the hero img
 jQuery(document).ready(function($) {
