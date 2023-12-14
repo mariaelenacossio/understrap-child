15310980f3ef54e20b097c54086e32c4e3d1b62b
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-12-14 15:26:12 -0800

    Address the non-passive event listener warning and to include { passive: true }

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -37,45 +37,57 @@ jQuery(document).ready(function($) {
 //Typing script for the roles
     const typedTextSpan = document.querySelector(".typed-text");
     const cursorSpan = document.querySelector(".cursor");
-    
+
     const textArray = ["Web Designer", "Web Developer", "Front end Developer", "UX/UI Designer"];
     const typingDelay = 200;
     const erasingDelay = 100;
-    const newTextDelay = 2000; // Delay between current and next text
+    const newTextDelay = 2000;
     let textArrayIndex = 0;
     let charIndex = 0;
-    
+
     function type() {
-      if (charIndex < textArray[textArrayIndex].length) {
-        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-        charIndex++;
-        setTimeout(type, typingDelay);
-      } 
-      else {
-        cursorSpan.classList.remove("typing");
-          setTimeout(erase, newTextDelay);
-      }
+        if (charIndex < textArray[textArrayIndex].length) {
+            if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+            typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, typingDelay);
+        } else {
+            cursorSpan.classList.remove("typing");
+            setTimeout(erase, newTextDelay);
+        }
     }
-    
+
     function erase() {
         if (charIndex > 0) {
-        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
-        charIndex--;
-        setTimeout(erase, erasingDelay);
-      } 
-      else {
-        cursorSpan.classList.remove("typing");
-        textArrayIndex++;
-        if(textArrayIndex>=textArray.length) textArrayIndex=0;
-        setTimeout(type, typingDelay + 1100);
-      }
+            if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+            typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+            charIndex--;
+            setTimeout(erase, erasingDelay);
+        } else {
+            cursorSpan.classList.remove("typing");
+            textArrayIndex++;
+            if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+            setTimeout(type, typingDelay + 1100);
+        }
     }
-    
-    document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-      if(textArray.length) setTimeout(type, newTextDelay + 250);
-    });    
+
+    document.addEventListener("DOMContentLoaded", function () {
+        if (textArray.length) setTimeout(type, newTextDelay + 250);
+    });
+
+    // Use a unified event listener for both desktop and mobile
+    document.addEventListener("pointerdown", startAnimation, { passive: true });
+
+    function startAnimation(event) {
+        // Check if it's a touch event
+        const isTouch = event.type === "touchstart";
+
+        // Start animation only if it's a touch event or the cursor is not typing
+        if (isTouch || charIndex === 0) {
+            type();
+        }
+    }
+  
     
 //Parallax effect for the hero img
 jQuery(document).ready(function($) {
