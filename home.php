671c382076788e8302347f40dd0414771b1cfed9
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-12-15 13:51:59 -0800

    Change link for resume

--- home.php
+++ home.php
@@ -74,12 +74,10 @@ $container = get_theme_mod( 'understrap_container_type' );
                             <p>I am a <span class="typed-text"></span><span class="cursor">&nbsp;</span></p>
                         </div><!--.role-container-->
                         <div class="d-grid gap-2 d-md-flex justify-content-md-center">
-                        <a class="btn btn-primary btn-lg px-4 me-md-2" href="#resume">Resume </a>  <!--.btn-resume-->
-
-                            <!-- <a href="#resume"
+                            <a href="#resume"
                                 target="_blank">
-                                <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Resume</button> -->
-                         
+                                <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Resume</button>
+                            </a><!--.btn-resume-->
                             <a href="#portfolio">
                                 <button type="button"
                                     class="btn btn-outline-secondary btn-lg px-4 me-md-2">Portfolio</button>
