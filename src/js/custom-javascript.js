617c3b696519c493b19ea9493941e3803c31a4a8
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-12-15 13:26:30 -0800

    Add a function to smoothen transition between sections

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -114,48 +114,19 @@ jQuery(document).ready(function ($) {
 });
 
 
-//     const typedTextSpan = document.querySelector(".typed-text");
-//     const cursorSpan = document.querySelector(".cursor");
-    
-//     const textArray = ["Web Designer", "Web Developer", "Front end Developer", "UX/UI Designer"];
-//     const typingDelay = 200;
-//     const erasingDelay = 100;
-//     const newTextDelay = 2000; // Delay between current and next text
-//     let textArrayIndex = 0;
-//     let charIndex = 0;
-    
-//     function type() {
-//       if (charIndex < textArray[textArrayIndex].length) {
-//         if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-//         typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-//         charIndex++;
-//         setTimeout(type, typingDelay);
-//       } 
-//     else {
-//         cursorSpan.classList.remove("typing");
-//           setTimeout(erase, newTextDelay);
-//       }
-//     }
-    
-//     function erase() {
-//         if (charIndex > 0) {
-//         if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-//         typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
-//         charIndex--;
-//         setTimeout(erase, erasingDelay);
-//       } 
-//       else {
-//         cursorSpan.classList.remove("typing");
-//         textArrayIndex++;
-//         if(textArrayIndex>=textArray.length) textArrayIndex=0;
-//         setTimeout(type, typingDelay + 1100);
-//       }
-//     }
-    
-//     document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-//       if(textArray.length) setTimeout(type, newTextDelay + 250);
-    
-// });    
+//For smooth transition between sections
+jQuery(document).ready(function ($) {
+    $('a[href*="#"]').on('click', function (event) {
+      event.preventDefault();
+      $('html, body').animate(
+        {
+          scrollTop: $($.attr(this, 'href')).offset().top
+        },
+        500
+      );
+    });
+  });
+  
 
 
 
