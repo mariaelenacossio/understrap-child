cf0e1bd430be0356a8a37b7f1bc1987565bde716
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-12-14 23:02:05 -0800

    Add missing character in link

--- page-templates/blog-post-1.php
+++ page-templates/blog-post-1.php
@@ -16,7 +16,7 @@ defined( 'ABSPATH' ) || exit;
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
     <?php
 get_header();
