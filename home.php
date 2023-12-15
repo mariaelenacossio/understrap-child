25bbcb622ca0934d7f933c010910be6a3f075517
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-12-14 23:23:18 -0800

    Test

--- home.php
+++ home.php
@@ -81,6 +81,70 @@ $container = get_theme_mod( 'understrap_container_type' );
 </div><!--.parallax-container-->
 <!--End of Hero Section-->
 
+<!--Interface Content-->
+<div class="interface">
+
+
+                <!-- About Me -->
+                <section id="about" class="bg-light py-4 py-md-5 py-xl-6">
+                <div class="container mb-4 mb-md-5 mb-xl-6">
+                  <div class="row justify-content-md-center">
+                    <div class="col-12 col-md-10 col-xxl-8">
+                      <img class="img-fluid rounded shadow" loading="eager" 
+                      src="https://mariaelena-cossioclark.com/wp-content/uploads/2023/11/design-system-desktop-mockup-3@2x.jpg" 
+                      alt="web design, website mockups, ux/ui design, portfolio design, digital design, responsive design, 
+                      creative web design, user interface design">
+                    </div>
+                  </div>
+                </div>
+                <div class="container overflow-hidden">
+                  <div class="row gy-2 gy-md-0 justify-content-xxl-center">
+                    <div class="col-12 order-md-1 col-md-8 col-xxl-6">
+                      <div class="text-center text-md-start">
+                        <h2 class="display-3 h2 fw-bold lh-1">Mariaelena Cossio Clark</h2>
+                        <p class="text-secondary fs-4 mb-2">Web Designer</p>
+                        <hr class="w-25 mx-auto ms-md-0 mb-4 text-secondary">
+                        <p>I am a passionate and organized individual who loves crafting digital stories through websites and apps. 
+                                  I have a keen eye for detail and utilize Adobe Creative Cloud, particularly Xd and Adobe Illustrator, in my creative process. 
+                                  Overcoming programming challenges, HTML, CSS, and JS have become my go-to tools for translating abstract ideas into reality. 
+                                  I thrive in a collaborative team environment, always eager to learn from others. 
+                                  Ultimately, my daily inspiration comes from creating meaningful digital experiences.</p>
+                      </div>
+                    </div>
+                    <div class="col-12 order-md-0 col-md-4 col-xxl-4">
+                      <div class="text-center text-md-start me-md-3 me-xl-5">
+                        <p class="mb-4">
+                          <span class="d-block display-6 lh-1">2+</span>
+                          <span class="fs-4 text-secondary">years of experience</span>
+                        </p>
+                        
+                        <!-- <div class="d-flex flex-row"> -->
+                        <div class="icon-test">
+                        <ul>
+                    <li class="social-icon">
+                      <a href="https://github.com/mariaelenacossio" target="_blank">
+                      <svg class="github-icon"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" aria-label="Github">
+                        <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                        <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>
+                      </a>
+                    </li>
+                    <li class="social-icon">
+                      <a href="https://www.linkedin.com/in/mariaelena-cossio-clark-320ba416b" target="_blank">
+                      <svg class="linkedin-icon"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" aria-label="Linkedin">
+                        <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                        <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                        <!-- </div> -->
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                </section>
+
+<!--Resume Section-->
 <div class="hero-container bg-light">
     <div class="window-container max-w-4xl mx-auto mt-10 shadow-lg">
         <div class="window-header p-4 d-flex justify-content-between align-items-center">
@@ -164,135 +228,7 @@ $container = get_theme_mod( 'understrap_container_type' );
     </div><!--.window-container-->
 </div><!--.hero-container-->
 
-
-<!--Interface Content-->
-<div class="interface">
-
-
-                <!-- About Me -->
-                <section id="about" class="bg-light py-4 py-md-5 py-xl-6">
-                <div class="container mb-4 mb-md-5 mb-xl-6">
-                  <div class="row justify-content-md-center">
-                    <div class="col-12 col-md-10 col-xxl-8">
-                      <img class="img-fluid rounded shadow" loading="eager" 
-                      src="https://mariaelena-cossioclark.com/wp-content/uploads/2023/11/design-system-desktop-mockup-3@2x.jpg" 
-                      alt="web design, website mockups, ux/ui design, portfolio design, digital design, responsive design, 
-                      creative web design, user interface design">
-                    </div>
-                  </div>
-                </div>
-                <div class="container overflow-hidden">
-                  <div class="row gy-2 gy-md-0 justify-content-xxl-center">
-                    <div class="col-12 order-md-1 col-md-8 col-xxl-6">
-                      <div class="text-center text-md-start">
-                        <h2 class="display-3 h2 fw-bold lh-1">Mariaelena Cossio Clark</h2>
-                        <p class="text-secondary fs-4 mb-2">Web Designer</p>
-                        <hr class="w-25 mx-auto ms-md-0 mb-4 text-secondary">
-                        <p>I am a passionate and organized individual who loves crafting digital stories through websites and apps. 
-                                  I have a keen eye for detail and utilize Adobe Creative Cloud, particularly Xd and Adobe Illustrator, in my creative process. 
-                                  Overcoming programming challenges, HTML, CSS, and JS have become my go-to tools for translating abstract ideas into reality. 
-                                  I thrive in a collaborative team environment, always eager to learn from others. 
-                                  Ultimately, my daily inspiration comes from creating meaningful digital experiences.</p>
-                      </div>
-                    </div>
-                    <div class="col-12 order-md-0 col-md-4 col-xxl-4">
-                      <div class="text-center text-md-start me-md-3 me-xl-5">
-                        <p class="mb-4">
-                          <span class="d-block display-6 lh-1">2+</span>
-                          <span class="fs-4 text-secondary">years of experience</span>
-                        </p>
-                        
-                        <!-- <div class="d-flex flex-row"> -->
-                        <div class="icon-test">
-                        <ul>
-                    <li class="social-icon">
-                      <a href="https://github.com/mariaelenacossio" target="_blank">
-                      <svg class="github-icon"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" aria-label="Github">
-                        <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                        <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>
-                      </a>
-                    </li>
-                    <li class="social-icon">
-                      <a href="https://www.linkedin.com/in/mariaelena-cossio-clark-320ba416b" target="_blank">
-                      <svg class="linkedin-icon"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" aria-label="Linkedin">
-                        <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                        <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                        <!-- </div> -->
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                </section>
-
-<!--Resume Section-->
-                <section id="resume" class="resume">
-      <div class="container">
-
-        <div class="section-title">
-          <h2>Resume</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-        </div>
-
-        <div class="row">
-          <div class="col-lg-6" data-aos="fade-up">
-            <h3 class="resume-title">Sumary</h3>
-            <div class="resume-item pb-0">
-              <h4>Alex Smith</h4>
-              <p><em>Innovative and deadline-driven Graphic Designer with 3+ years of experience designing and developing user-centered digital/print marketing material from initial concept to final, polished deliverable.</em></p>
-              <ul>
-                <li>Portland par 127,Orlando, FL</li>
-                <li>(123) 456-7891</li>
-                <li>alice.barkley@example.com</li>
-              </ul>
-            </div>
-
-            <h3 class="resume-title">Education</h3>
-            <div class="resume-item">
-              <h4>Master of Fine Arts &amp; Graphic Design</h4>
-              <h5>2015 - 2016</h5>
-              <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-              <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend</p>
-            </div>
-            <div class="resume-item">
-              <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
-              <h5>2010 - 2014</h5>
-              <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-              <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p>
-            </div>
-          </div>
-          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-            <h3 class="resume-title">Professional Experience</h3>
-            <div class="resume-item">
-              <h4>Senior graphic design specialist</h4>
-              <h5>2019 - Present</h5>
-              <p><em>Experion, New York, NY </em></p>
-              <ul>
-                <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
-                <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project. </li>
-                <li>Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design</li>
-                <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000</li>
-              </ul>
-            </div>
-            <div class="resume-item">
-              <h4>Graphic design specialist</h4>
-              <h5>2017 - 2018</h5>
-              <p><em>Stepping Stone Advertising, New York, NY</em></p>
-              <ul>
-                <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
-                <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
-                <li>Recommended and consulted with clients on the most appropriate graphic design</li>
-                <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-
-      </div>
-    </section><!-- End Resume Section -->
+<!-- End Resume Section -->
 
 
 
