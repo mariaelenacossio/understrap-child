4d73ac775240ae3299f7e251b1aeae1d87a42c63
mariaelenacossio <98018018+mariaelenacossio@users.noreply.github.com>
2023-12-14 18:01:36 -0800

    Remove changes

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -35,59 +35,48 @@ jQuery(document).ready(function($) {
 });
 
 //Typing script for the roles
-jQuery(document).ready(function ($) {
-    console.log("DOM fully loaded");
-
-    // Your existing code here
-    const typedTextSpan = $(".typed-text");
-    const cursorSpan = $(".cursor");
-
+    const typedTextSpan = document.querySelector(".typed-text");
+    const cursorSpan = document.querySelector(".cursor");
+    
     const textArray = ["Web Designer", "Web Developer", "Front end Developer", "UX/UI Designer"];
     const typingDelay = 200;
     const erasingDelay = 100;
-    const newTextDelay = 2000;
+    const newTextDelay = 2000; // Delay between current and next text
     let textArrayIndex = 0;
     let charIndex = 0;
-    let isTyping = true;
-
+    
     function type() {
-        if (charIndex < textArray[textArrayIndex].length) {
-            if (!cursorSpan.hasClass("typing")) cursorSpan.addClass("typing");
-            typedTextSpan.text(typedTextSpan.text() + textArray[textArrayIndex].charAt(charIndex));
-            charIndex++;
-            setTimeout(type, typingDelay);
-        } else {
-            cursorSpan.removeClass("typing");
-            isTyping = false;
-            setTimeout(erase, newTextDelay);
-        }
+      if (charIndex < textArray[textArrayIndex].length) {
+        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+      } 
+      else {
+        cursorSpan.classList.remove("typing");
+          setTimeout(erase, newTextDelay);
+      }
     }
-
+    
     function erase() {
         if (charIndex > 0) {
-            if (!cursorSpan.hasClass("typing")) cursorSpan.addClass("typing");
-            typedTextSpan.text(textArray[textArrayIndex].substring(0, charIndex - 1));
-            charIndex--;
-            setTimeout(erase, erasingDelay);
-        } else {
-            cursorSpan.removeClass("typing");
-            isTyping = true;
-            textArrayIndex++;
-            if (textArrayIndex >= textArray.length) textArrayIndex = 0;
-            setTimeout(type, typingDelay + 1100);
-        }
+        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+      } 
+      else {
+        cursorSpan.classList.remove("typing");
+        textArrayIndex++;
+        if(textArrayIndex>=textArray.length) textArrayIndex=0;
+        setTimeout(type, typingDelay + 1100);
+      }
     }
-
-    // Start typing animation only if the typedTextSpan is found
-    if (typedTextSpan.length) {
-        if (textArray.length) setTimeout(type, newTextDelay + 250);
-    }
-});
-
-
-
-
-
+    
+    document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+      if(textArray.length) setTimeout(type, newTextDelay + 250);
+    });    
+    
 //Parallax effect for the hero img
 jQuery(document).ready(function($) {
     var lastScrollTop = 0;
